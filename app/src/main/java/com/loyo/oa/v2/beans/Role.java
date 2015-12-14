a2775e7e68faf2859452205bb36c033a970ee703
yaojinze <yjz@360loyo.com>
2015-12-14 17:22:13 +0800

    通讯录－本部门列表页面字段只有员工的部门，缺失“职位”字段信息 通讯详情界面也要有职位信息，界面样式参考UI

--- app/src/main/java/com/loyo/oa/v2/beans/Role.java
+++ app/src/main/java/com/loyo/oa/v2/beans/Role.java
@@ -8,7 +8,11 @@ public class Role implements Serializable {
     public final static int ALL = 1;
     public final static int DEPT_AND_CHILD = 2;
     public final static int SELF = 3;
-    private int dataRange;
+    public String id;
+    public String name;
+    public int dataRange;
+    public int  customerNumLimit;
+
 
     public int getDataRange() {
         return dataRange == 0 ? SELF : dataRange;
