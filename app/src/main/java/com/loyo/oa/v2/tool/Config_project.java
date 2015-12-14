977cbf593189594faccf6c818cd5079ac5323edd
yaojinze <yjz@360loyo.com>
2015-12-14 11:41:37 +0800

    换环境

--- app/src/main/java/com/loyo/oa/v2/tool/Config_project.java
+++ app/src/main/java/com/loyo/oa/v2/tool/Config_project.java
@@ -8,10 +8,10 @@ import com.loyo.oa.v2.BuildConfig;
 public class Config_project {
 
     //public static final Boolean is_developer_mode = false; //生产模式
-    public static String IP="http://114.215.83.227";
+    //public static String IP="http://114.215.83.227";
 
     public static final Boolean is_developer_mode = BuildConfig.DEBUG;
-    //public static String IP = "http://192.168.31.131";//内部服务器
+    public static String IP = "http://192.168.31.131";//内部服务器
 
 
     public static String API_URL() {
