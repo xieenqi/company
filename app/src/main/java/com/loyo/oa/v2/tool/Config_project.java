2ef10a991af9fd081d9bdb6e97bc4d70e75c1918
yaojinze <yjz@360loyo.com>
2016-01-14 20:20:20 +0800

    企业qq的绑定手机问题

--- app/src/main/java/com/loyo/oa/v2/tool/Config_project.java
+++ app/src/main/java/com/loyo/oa/v2/tool/Config_project.java
@@ -13,9 +13,9 @@ public class Config_project {
 
     public static final Boolean is_developer_mode = BuildConfig.DEBUG;//dbug模式
 
-    public static String IP = "http://192.168.31.131";     //内部测试环境
+    //public static String IP = "http://192.168.31.131";     //内部测试环境
 
-    //public static String IP = "http://staging.ukuaiqi.com";  //产品预上线环境
+    public static String IP = "http://staging.ukuaiqi.com";  //产品预上线环境
 
     //public static String IP = "http://112.74.66.99";       //产品预上线环境
 
@@ -88,9 +88,9 @@ public class Config_project {
      */
     public static String SERVER_URL_LOGIN() {
 
-        return isRelease ? Domain.account : IP + ":8080";
+        return isRelease ? Domain.account : IP + ":88";
     }
-    //131服务器 88端口
+    //产品预上线环境服务器 88端口  131的是8080
 
     /**
      * oa 系统
--- app/src/main/java/com/loyo/oa/v2/tool/MyLayoutUtils.java
+++ app/src/main/java/com/loyo/oa/v2/tool/MyLayoutUtils.java
@@ -6,25 +6,24 @@ import android.support.v7.widget.RecyclerView;
 import android.view.View;
 
 
-
-    public class MyLayoutUtils extends LinearLayoutManager {
-
-
-        public MyLayoutUtils(Context context) {
-            super(context);
-            // TODO Auto-generated constructor stub
-        }
+public class MyLayoutUtils extends LinearLayoutManager {
 
 
-        @Override
-        public void onMeasure(RecyclerView.Recycler recycler, RecyclerView.State state, int widthSpec,int heightSpec) {
-            View view = recycler.getViewForPosition(0);
-            if(view != null){
-                measureChild(view, widthSpec, heightSpec);
-                int measuredWidth = View.MeasureSpec.getSize(widthSpec);
-                int measuredHeight = view.getMeasuredHeight();
-                setMeasuredDimension(measuredWidth, measuredHeight);
-            }
+    public MyLayoutUtils(Context context) {
+        super(context);
+        // TODO Auto-generated constructor stub
+    }
+
+
+    @Override
+    public void onMeasure(RecyclerView.Recycler recycler, RecyclerView.State state, int widthSpec, int heightSpec) {
+        View view = recycler.getViewForPosition(0);
+        if (view != null) {
+            measureChild(view, widthSpec, heightSpec);
+            int measuredWidth = View.MeasureSpec.getSize(widthSpec);
+            int measuredHeight = view.getMeasuredHeight();
+            setMeasuredDimension(measuredWidth, measuredHeight);
         }
     }
+}
 
