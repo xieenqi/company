de9d4bbea661e025ddd91eec1a3f8f621506f2dd
曾科 <zke@360loyo.com>
2016-01-15 20:49:18 +0800

    接口合并提交

--- app/src/main/java/com/loyo/oa/v2/tool/Config_project.java
+++ app/src/main/java/com/loyo/oa/v2/tool/Config_project.java
@@ -13,13 +13,13 @@ public class Config_project {
 
     public static final Boolean is_developer_mode = BuildConfig.DEBUG;//dbug模式
 
-    public static String IP = "http://192.168.31.131";     //内部测试环境
+    //public static String IP = "http://192.168.31.131";     //内部测试环境
 
     //public static String IP = "http://staging.ukuaiqi.com";  //产品预上线环境
 
     //public static String IP = "http://112.74.66.99";       //产品预上线环境
 
-    //public static String IP = "http://ukuaiqi.com";        //产品正式环境
+    public static String IP = "http://ukuaiqi.com";        //产品正式环境
 
     /**
      * 正式产品的域名
@@ -36,11 +36,11 @@ public class Config_project {
         /**
          * oa 系统
          */
-        public static String oa = "http://oa.kuaiqi.com";
+        public static String oa = "http://oa.ukuaiqi.com";
         /**
          * attachment 附件
          */
-        public static String attachment = "http://attachment.kuaiqi.com";
+        public static String attachment = "http://attachment.ukuaiqi.com";
         /**
          * discuss 讨论
          */
