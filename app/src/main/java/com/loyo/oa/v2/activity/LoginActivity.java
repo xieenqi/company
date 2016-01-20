bebd22859743febc90b113fa60ef1f21a6273bf8
yaojinze <yjz@360loyo.com>
2016-01-20 19:35:14 +0800

    gas 高斯优化

--- app/src/main/java/com/loyo/oa/v2/activity/LoginActivity.java
+++ app/src/main/java/com/loyo/oa/v2/activity/LoginActivity.java
@@ -65,8 +65,8 @@ public class LoginActivity extends BaseActivity implements View.OnClickListener,
         edt_password.addTextChangedListener(nameWatcher);
         layout_login.setOnClickListener(this);
         if (Config_project.is_developer_mode) {
-            /*edt_username.setText("17780704580");
-            edt_password.setText("123456");*/
+            edt_username.setText("17780704580");
+            edt_password.setText("123456");
         }
     }
 
