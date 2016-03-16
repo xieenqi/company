f018db61ff46af1454ec0e22bc4b6a53bf2ae910
l612714 <1043973279@qq.com>
2016-03-16 11:39:05 +0800

    checkstyle CustomerContractAddActivity.class

--- app/src/main/java/com/loyo/oa/v2/activity/customer/CustomerContractAddActivity.java
+++ app/src/main/java/com/loyo/oa/v2/activity/customer/CustomerContractAddActivity.java
@@ -36,7 +36,7 @@ import retrofit.client.Response;
 
 /**
  * 客户管理联系人列表，【新增联系人】
- * */
+ */
 
 public class CustomerContractAddActivity extends BaseActivity implements View.OnClickListener {
 
@@ -49,7 +49,7 @@ public class CustomerContractAddActivity extends BaseActivity implements View.On
     Contact mContact;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_customer_contract_add);
 
@@ -104,7 +104,7 @@ public class CustomerContractAddActivity extends BaseActivity implements View.On
     }
 
     @Override
-    public void onClick(View v) {
+    public void onClick(final View v) {
         switch (v.getId()) {
             case R.id.img_title_left:
                 app.finishActivity(this, MainApp.ENTER_TYPE_LEFT, RESULT_CANCELED, null);
@@ -119,22 +119,22 @@ public class CustomerContractAddActivity extends BaseActivity implements View.On
                 if (phone.isEmpty()) {
                     Toast("手机号不能为空");
                     return;
-                }else{//验证电话号码
-                    if(!RegularCheck.isMobilePhone(phone)){
+                } else {//验证电话号码
+                    if (!RegularCheck.isMobilePhone(phone)) {
                         Toast("手机号码格式不正确");
                         return;
                     }
                 }
                 //如果有输入座机就验证座机
-                if(!TextUtils.isEmpty(getEditTextContent(edt_wiretel))){
-                    if(!RegularCheck.isPhone(getEditTextContent(edt_wiretel))){
+                if (!TextUtils.isEmpty(getEditTextContent(edt_wiretel))) {
+                    if (!RegularCheck.isPhone(getEditTextContent(edt_wiretel))) {
                         Toast("座机号码格式不正确");
                         return;
                     }
                 }
                 //邮箱验证
-                if(!TextUtils.isEmpty(getEditTextContent(edt_email))){
-                    if(!RegularCheck.checkEmail(getEditTextContent(edt_email))){
+                if (!TextUtils.isEmpty(getEditTextContent(edt_email))) {
+                    if (!RegularCheck.checkEmail(getEditTextContent(edt_email))) {
                         Toast("邮箱格式不正确");
                         return;
                     }
@@ -146,30 +146,30 @@ public class CustomerContractAddActivity extends BaseActivity implements View.On
                 maps.put("wiretel", getEditTextContent(edt_wiretel));
                 String birth = edt_birth.getText().toString().trim();
                 if (!TextUtils.isEmpty(birth)) {
-                    maps.put("birth",birth);
+                    maps.put("birth", birth);
                 }// DateTool.getDateToTimestamp(birth, app.df4) / 1000+""
                 maps.put("wx", getEditTextContent(edt_wx));
                 maps.put("qq", getEditTextContent(edt_qq));
                 maps.put("email", getEditTextContent(edt_email));
                 maps.put("memo", getEditTextContent(edt_memo));
-                LogUtil.d("添加联系人："+MainApp.gson.toJson(maps));
+                LogUtil.d("添加联系人：" + MainApp.gson.toJson(maps));
                 if (mCustomer != null) {
                     if (mContact == null) {
                         RestAdapterFactory.getInstance().build(Config_project.API_URL_CUSTOMER()).create(ICustomer.class).addContact(mCustomer.getId(), maps, new RCallback<Contact>() {
                             @Override
-                            public void success(Contact contact, Response response) {
+                            public void success(final Contact contact, final Response response) {
                                 mContact = contact;
                                 sendBack();
                             }
-                            @Override
-                            public void failure(RetrofitError error) {
-                                LogUtil.d("客户 新增联系人URL"+error.getUrl());
-                                LogUtil.d("客户 新增联系人"+error.getMessage());
 
-                                if(error.getKind() == RetrofitError.Kind.NETWORK){
+                            @Override
+                            public void failure(final RetrofitError error) {
+                                LogUtil.d("客户 新增联系人URL" + error.getUrl());
+                                LogUtil.d("客户 新增联系人" + error.getMessage());
+
+                                if (error.getKind() == RetrofitError.Kind.NETWORK) {
                                     Toast("请检查您的网络连接");
-                                }
-                                else if(error.getResponse().getStatus() == 500){
+                                } else if (error.getResponse().getStatus() == 500) {
                                     Toast("网络异常500,请稍候再试");
                                 }
                             }
@@ -178,16 +178,16 @@ public class CustomerContractAddActivity extends BaseActivity implements View.On
                         /*修改联系人*/
                         RestAdapterFactory.getInstance().build(Config_project.API_URL_CUSTOMER()).create(ICustomer.class).updateContact(mCustomer.getId(), mContact.getId(), maps, new RCallback<Contact>() {
                             @Override
-                            public void success(Contact contact, Response response) {
+                            public void success(final Contact contact, final Response response) {
                                 mContact = contact;
                                 sendBack();
                             }
 
                             @Override
-                            public void failure(RetrofitError error) {
+                            public void failure(final RetrofitError error) {
 
-                                LogUtil.d("客户 修改联系人:"+error.getMessage());
-                                LogUtil.d("客户 修改联系人URL:"+error.getUrl());
+                                LogUtil.d("客户 修改联系人:" + error.getMessage());
+                                LogUtil.d("客户 修改联系人URL:" + error.getUrl());
 
                                 if (error.getKind() == RetrofitError.Kind.NETWORK) {
                                     Toast("请检查您的网络连接");
@@ -204,6 +204,9 @@ public class CustomerContractAddActivity extends BaseActivity implements View.On
             case R.id.edt_birthday:
                 selectBirthDay();
                 break;
+            default:
+
+                break;
         }
     }
 
@@ -214,7 +217,7 @@ public class CustomerContractAddActivity extends BaseActivity implements View.On
         DateTool.calendar = Calendar.getInstance();
         DatePickerDialog datePickerDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
             @Override
-            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+            public void onDateSet(final DatePicker view, final int year, final int monthOfYear, final int dayOfMonth) {
                 int age = Utils.getAge(year + "");
                 if (age > 0) {
                     String str = year + "." + String.format("%02d", (monthOfYear + 1)) + "." + String.format("%02d", dayOfMonth);
@@ -245,7 +248,7 @@ public class CustomerContractAddActivity extends BaseActivity implements View.On
      * @param et 需要获取数据的EditText
      * @return EditText里的内容
      */
-    private String getEditTextContent(EditText et) {
+    private String getEditTextContent(final EditText et) {
         return et.getText().toString().trim();
     }
 
@@ -266,7 +269,7 @@ public class CustomerContractAddActivity extends BaseActivity implements View.On
     public class AsyncHandler_contract extends BaseActivityAsyncHttpResponseHandler {
 
         @Override
-        public void onSuccess(int arg0, Header[] arg1, byte[] arg2) {
+        public void onSuccess(final int arg0, final Header[] arg1, final byte[] arg2) {
             try {
                 Contact contact = MainApp.gson.fromJson(getStr(arg2), Contact.class);
                 if (contact != null) {
