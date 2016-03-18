46a3c618a0a41989d79d675b3a176db97a10dfec
xeq <yjz@360loyo.com>
2016-03-18 18:11:01 +0800

    合并代码

--- app/src/main/java/com/loyo/oa/v2/common/ExtraAndResult.java
+++ app/src/main/java/com/loyo/oa/v2/common/ExtraAndResult.java
@@ -14,6 +14,7 @@ public class ExtraAndResult {
     public static final String EXTRA_TYPE = "extra_type";
     public static final String EXTRA_UUID = "extra_uuid";
     public static final String EXTRA_TYPE_ID = "extra_type_id";
+    public static final String EXTRA_BOOLEAN = "extra_boolean";
 
     public static final String RESULT_ID = "result_id";
     public static final String RESULT_NAME = "result_name";
