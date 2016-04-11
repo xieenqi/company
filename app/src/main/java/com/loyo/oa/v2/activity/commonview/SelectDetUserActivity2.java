43c42146f53af62a096065557c647f1191b4c315
xeq <yjz@360loyo.com>
2016-04-11 10:29:34 +0800

    修复公用选人控件 bug

--- app/src/main/java/com/loyo/oa/v2/activity/commonview/SelectDetUserActivity2.java
+++ app/src/main/java/com/loyo/oa/v2/activity/commonview/SelectDetUserActivity2.java
@@ -422,6 +422,9 @@ public class SelectDetUserActivity2 extends BaseActivity implements View.OnClick
                 SelectUserData data = SelectUserHelper.mCurrentSelectDatas.get(position);
                 if (data.getClass() == SelectDepData.class) {
                     ((SelectDepData) data).setAllSelect(false);
+                } else if (data.getClass() == SelectUserData.class) {
+                    ((SelectUserData) data).setCallbackSelect(false);
+                    mSelectUsersAdapter.notifyDataSetChanged();
                 } else {
                     data.setCallbackSelect(false);
                 }
