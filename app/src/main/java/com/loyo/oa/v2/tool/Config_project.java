f1082bf550b2c013e524e96e1356a70812578aab
应雨洋 <peiper341@gmail.com>
2016-04-25 17:54:55 +0800

    修复新建审批，判断出差请假开始结束时间bug

--- app/src/main/java/com/loyo/oa/v2/tool/Config_project.java
+++ app/src/main/java/com/loyo/oa/v2/tool/Config_project.java
@@ -8,15 +8,15 @@ import com.loyo.oa.v2.BuildConfig;
  */
 public class Config_project {
 
-    public static boolean isRelease = false; //是否是正式产品
+    public static boolean isRelease = true; //是否是正式产品
 
     //public static final Boolean is_developer_mode = false; //生产模式
 
     public static final Boolean is_developer_mode = BuildConfig.DEBUG; //dbug模式`
 
-    //    public static String IP = "http://192.168.31.131";        //内部测试环境
+    public static String IP = "http://192.168.31.131";        //内部测试环境
 
-    public static String IP = "http://staging.ukuaiqi.com"; //产品预上线环境
+    //public static String IP = "http://staging.ukuaiqi.com"; //产品预上线环境
 
     //public static String IP = "http://112.74.66.99";        //产品预上线环境
 
@@ -36,11 +36,12 @@ public class Config_project {
     private static class Domain {
         /**
          * 账号中心
+         * oa
          */
         public static String account = "http://user.ukuaiqi.com";
-
         /**
          * crm 客户管理
+         * oa
          */
         public static String crm = "http://crm.ukuaiqi.com";
         /**
@@ -54,6 +55,7 @@ public class Config_project {
         public static String attachment = "http://attachment.ukuaiqi.com";
         /**
          * discuss 讨论
+         * oa
          */
         public static String discuss = "http://discuss.ukuaiqi.com";
         /**
@@ -78,12 +80,9 @@ public class Config_project {
 
     /**
      * 客户地址
-     * <p/>
-     * staging端口号依然是:8090
-     * 131 136等内网端口号:8070
      */
     public static String SERVER_URL_CUSTOMER() {
-        return isRelease ? Domain.crm : IP + ":8090";
+        return isRelease ? Domain.crm : IP + ":8070";
     }
 
     /**
