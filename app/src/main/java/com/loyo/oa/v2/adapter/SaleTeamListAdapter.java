11c5e32bad288e92621a82644b6775af62e297a1
应雨洋 <peiper341@gmail.com>
2016-05-17 19:05:33 +0800

    代码合并

---
+++ app/src/main/java/com/loyo/oa/v2/adapter/SaleTeamListAdapter.java
@@ -0,0 +1,31 @@
+package com.loyo.oa.v2.adapter;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+/**
+ * 我的团队 销售机会 adapter
+ * Created by yyy on 16/5/17.
+ */
+public class SaleTeamListAdapter extends BaseAdapter {
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return null;
+    }
+}
