2aeeb05f8525627aade68ab6902a130d7d819652
xeq <yjz@360loyo.com>
2016-05-17 20:17:37 +0800

    创建销售机会 页面搭建完成

---
+++ app/src/main/java/com/loyo/oa/v2/activity/sale/ActivityAddMySale.java
@@ -0,0 +1,49 @@
+package com.loyo.oa.v2.activity.sale;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.loyo.oa.v2.R;
+import com.loyo.oa.v2.common.Global;
+import com.loyo.oa.v2.tool.BaseActivity;
+
+/**
+ * 【创建销售机会】
+ * Created by xeq on 16/5/17.
+ */
+public class ActivityAddMySale extends BaseActivity {
+    private TextView tv_title;
+    private ImageView iv_submit;
+    private LinearLayout ll_back;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_my_sale);
+        init();
+    }
+
+    private void init() {
+        tv_title = (TextView) findViewById(R.id.tv_title);
+        tv_title.setText("创建销售机会");
+        iv_submit = (ImageView) findViewById(R.id.iv_submit);
+        iv_submit.setImageResource(R.drawable.right_submit1);
+        ll_back = (LinearLayout) findViewById(R.id.ll_back);
+        ll_back.setOnTouchListener(Global.GetTouch());
+        ll_back.setOnClickListener(click);
+    }
+
+    private View.OnClickListener click = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()) {
+                case R.id.ll_back:
+                    finish();
+                    break;
+            }
+        }
+    };
+}
