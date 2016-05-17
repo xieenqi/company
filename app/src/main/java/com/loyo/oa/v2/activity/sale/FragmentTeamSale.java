8d21daa310bde26e46dcd1d377bd9aa5d98327d7
xeq <yjz@360loyo.com>
2016-05-17 18:17:01 +0800

    销售机会 模块框架搭建  完成

---
+++ app/src/main/java/com/loyo/oa/v2/activity/sale/FragmentTeamSale.java
@@ -0,0 +1,33 @@
+package com.loyo.oa.v2.activity.sale;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.loyo.oa.v2.R;
+import com.loyo.oa.v2.tool.BaseFragment;
+
+/**
+ * 【团队的销售机会】
+ * Created by xeq on 16/5/17.
+ */
+public class FragmentTeamSale extends BaseFragment {
+
+    private View mView;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        if (null == mView) {
+            mView = inflater.inflate(R.layout.fragment_team_sale, null);
+        }
+        return mView;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
