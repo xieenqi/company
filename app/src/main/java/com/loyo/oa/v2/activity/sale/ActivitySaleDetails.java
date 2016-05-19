13fcec76387db919f056be9bcb999e8be16caafd
应雨洋 <peiper341@gmail.com>
2016-05-19 18:26:38 +0800

    机会详情，菜单弹窗完成

--- app/src/main/java/com/loyo/oa/v2/activity/sale/ActivitySaleDetails.java
+++ app/src/main/java/com/loyo/oa/v2/activity/sale/ActivitySaleDetails.java
@@ -1,18 +1,23 @@
 package com.loyo.oa.v2.activity.sale;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import com.loyo.oa.v2.R;
+import com.loyo.oa.v2.activity.SelectEditDeleteActivity;
 import com.loyo.oa.v2.tool.BaseActivity;
 
 /**
  * 机会详情
  * Created by yyy on 16/5/19.
  */
-public class ActivitySaleDetails extends BaseActivity {
+public class ActivitySaleDetails extends BaseActivity implements View.OnClickListener{
 
+    private final int EDIT_POP_WINDOW = 500;
     private LinearLayout img_title_left;
+    private RelativeLayout img_title_right;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,19 +27,47 @@ public class ActivitySaleDetails extends BaseActivity {
         initView();
     }
 
-    public void initView(){
-        img_title_left = (LinearLayout)findViewById(R.id.img_title_left);
-        img_title_left.setOnClickListener(click);
+    public void initView() {
+        mContext = this;
+        img_title_left = (LinearLayout) findViewById(R.id.img_title_left);
+        img_title_right = (RelativeLayout) findViewById(R.id.img_title_right);
+        img_title_left.setOnClickListener(this);
+        img_title_right.setOnClickListener(this);
     }
 
-    private View.OnClickListener click = new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            switch (v.getId()){
-                case R.id.img_title_left:
-                    finish();
-                    break;
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+
+            case R.id.img_title_left:
+                finish();
+                break;
+
+            case R.id.img_title_right:
+                Intent intent = new Intent(mContext, ActivitySaleEditView.class);
+                startActivityForResult(intent,EDIT_POP_WINDOW);
+                break;
+
+            default:
+                break;
+
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(resultCode != RESULT_OK){
+            return;
+        }
+
+        if(requestCode == EDIT_POP_WINDOW){
+            if(data.getBooleanExtra("edit",false)){
+                Toast("编辑回调");
+            }else if(data.getBooleanExtra("delete",false)){
+                Toast("删除回调");
             }
         }
-    };
+    }
 }
