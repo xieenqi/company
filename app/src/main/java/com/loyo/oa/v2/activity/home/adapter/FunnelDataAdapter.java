db5a26740954a665fa290d2b0158344ec072727e
xeq <yjz@360loyo.com>
2016-06-17 15:03:21 +0800

    销售漏斗 UI一次 完成

---
+++ app/src/main/java/com/loyo/oa/v2/activity/home/adapter/FunnelDataAdapter.java
@@ -0,0 +1,46 @@
+package com.loyo.oa.v2.activity.home.adapter;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import com.loyo.oa.v2.R;
+
+/**
+ * 添加 销售漏斗 数据
+ * Created by xeq on 16/6/16.
+ */
+public class FunnelDataAdapter extends LinearLayout {
+    private Context mContext;
+    private int[] colors = {R.drawable.img_funnel0, R.drawable.img_funnel1, R.drawable.img_funnel2, R.drawable.img_funnel3,
+            R.drawable.img_funnel4, R.drawable.img_funnel5, R.drawable.img_funnel6,
+            R.drawable.img_funnel7, R.drawable.img_funnel8, R.drawable.img_funnel9,
+    };
+//    private int index;
+
+    public FunnelDataAdapter(Context context, AttributeSet attrs, int defStyleAttr,int index) {
+        super(context, attrs, defStyleAttr);
+        mContext = context;
+        setLayoutParams(new ViewGroup.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT));
+        setOrientation(VERTICAL);
+        setPadding(0, 5, 0, 5);
+        bindView(index);
+    }
+
+    public FunnelDataAdapter(Context context, int index) {
+        this(context, null, 0,index);
+//        this.index = index;
+
+    }
+
+    private void bindView(int index) {
+        final View view = LayoutInflater.from(mContext).inflate(R.layout.item_funnel, null, false);
+        ImageView iv_img = (ImageView) view.findViewById(R.id.iv_img);
+        iv_img.setImageResource(colors[index]);
+        this.addView(view);
+    }
+}
--- app/src/main/java/com/loyo/oa/v2/activity/home/fragment/FragmentHomeStatistics.java
+++ app/src/main/java/com/loyo/oa/v2/activity/home/fragment/FragmentHomeStatistics.java
@@ -1,21 +1,17 @@
 package com.loyo.oa.v2.activity.home.fragment;
 
-import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.loyo.oa.v2.R;
+import com.loyo.oa.v2.activity.home.adapter.FunnelDataAdapter;
 import com.loyo.oa.v2.activity.home.adapter.ProcessDataAdapter;
-import com.loyo.oa.v2.activity.home.cusview.incrementview.Bar;
-import com.loyo.oa.v2.activity.home.cusview.incrementview.BarChartView;
-import com.loyo.oa.v2.activity.home.cusview.incrementview.BarSet;
-import com.loyo.oa.v2.activity.home.cusview.incrementview.DataRetriever;
-import com.loyo.oa.v2.activity.home.cusview.incrementview.YController;
 
 /**
  * 【统计】fragment
@@ -23,8 +19,9 @@ import com.loyo.oa.v2.activity.home.cusview.incrementview.YController;
 public class FragmentHomeStatistics extends Fragment {
 
     private final static String[] tracyColors = {"#f8668a", "#4ec469", "#4ddac2", "#31cbe8", "#88b9f7", "#7fcaff", "#f18f73", "#fdb485", "#fde068", "#12db8a"};
-    private LinearLayout ll_process;
-    private BarChartView bcv_view;
+    private LinearLayout ll_process, ll_funnel;
+    //    private BarChartView bcv_view;
+    ProgressBar pb_progress_vertical;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -40,61 +37,72 @@ public class FragmentHomeStatistics extends Fragment {
         return view;
     }
 
-    private void initView(View view) {//@drawable/shape_progressbar_mini
+    private void initView(View view) {
         ll_process = (LinearLayout) view.findViewById(R.id.ll_process);
-        bcv_view = (BarChartView) view.findViewById(R.id.bcv_view);
+        ll_funnel = (LinearLayout) view.findViewById(R.id.ll_funnel);
+//        bcv_view = (BarChartView) view.findViewById(R.id.bcv_view);
+        pb_progress_vertical = (ProgressBar) view.findViewById(R.id.pb_progress_vertical);
         for (int i = 0; i < 5; i++) {
             ll_process.addView(new ProcessDataAdapter(getActivity()));
         }
-        setIncrementData(1, 3);
+//        setIncrementData(1, 3);
+        pb_progress_vertical.setProgress(46);
+        pb_progress_vertical.setProgressDrawable(getResources().getDrawable(R.drawable.shape_progressbar_vertical1));
+        setFunnelData();
+    }
+
+    private void setFunnelData() {
+        for (int i = 0; i < 10; i++) {
+            ll_funnel.addView(new FunnelDataAdapter(getActivity(), i));
+        }
     }
 
     private final static String[] mLabels = {"新增客户数(12)", "新增机会数(23)", "新增合同数(32)"};
     private final static String[] colors = {"#7fcaff", "#feb98d", "#fde068"};
     private final static float[] valus = {56, 93, 61};
 
-    private void setIncrementData(int nSets, int nPoints) {
-        BarSet data;//条形
-        Bar bar;
-        bcv_view.reset();
-        for (int i = 0; i < nSets; i++) {//循环几次创建几个条形
-
-            data = new BarSet();
-            for (int j = 0; j < nPoints; j++) {
-                bar = new Bar(mLabels[j], valus[j]);//条形的名字  和 值
-                bar.setColor(Color.parseColor(colors[j]));
-                data.addBar(bar);
-//                data.setColor(Color.parseColor(colors[j]));//条形的颜色
-            }
-            bcv_view.addData(data);
-        }
-
-        bcv_view.setBarSpacing(200);//间隔宽度
-        bcv_view.setBarBackground(true);
-        bcv_view.setBarBackgroundColor(Color.parseColor("#f4f8fe"));
-        bcv_view.setRoundCorners(1);//设置圆角
-
-        bcv_view.setBorderSpacing(60)//条形到两边的边框的距离
-                .setFontSize(30)
-                .setLabelColor(Color.parseColor("#999999"))//字体颜色
-//                .setGrid(DataRetriever.randPaint())
-//                .setHorizontalGrid(DataRetriever.randPaint())
-//                .setVerticalGrid(DataRetriever.randPaint())
-                .setYLabels(YController.LabelPosition.NONE)//竖的是否显示
-//                .setYAxis(true)
-                .setXLabels(DataRetriever.getXPosition())
-//                .setXAxis(DataRetriever.randBoolean())
-//                        .setThresholdLine(2, DataRetriever.randPaint())
-                .setMaxAxisValue(100 * nSets, 20)//最大值 单位值
-                .animate(DataRetriever.randAnimation(new Runnable() {
-                    @Override
-                    public void run() {
-
-                    }
-                }, 3))
-        //.show()
-        ;
-    }
+//    private void setIncrementData(int nSets, int nPoints) {
+//        BarSet data;//条形
+//        Bar bar;
+//        bcv_view.reset();
+//        for (int i = 0; i < nSets; i++) {//循环几次创建几个条形
+//
+//            data = new BarSet();
+//            for (int j = 0; j < nPoints; j++) {
+//                bar = new Bar(mLabels[j], valus[j]);//条形的名字  和 值
+//                bar.setColor(Color.parseColor(colors[j]));
+//                data.addBar(bar);
+////                data.setColor(Color.parseColor(colors[j]));//条形的颜色
+//            }
+//            bcv_view.addData(data);
+//        }
+//
+//        bcv_view.setBarSpacing(200);//间隔宽度
+//        bcv_view.setBarBackground(true);
+//        bcv_view.setBarBackgroundColor(Color.parseColor("#f4f8fe"));
+//        bcv_view.setRoundCorners(1);//设置圆角
+//
+//        bcv_view.setBorderSpacing(60)//条形到两边的边框的距离
+//                .setFontSize(30)
+//                .setLabelColor(Color.parseColor("#999999"))//字体颜色
+////                .setGrid(DataRetriever.randPaint())
+////                .setHorizontalGrid(DataRetriever.randPaint())
+////                .setVerticalGrid(DataRetriever.randPaint())
+//                .setYLabels(YController.LabelPosition.NONE)//竖的是否显示
+////                .setYAxis(true)
+//                .setXLabels(DataRetriever.getXPosition())
+////                .setXAxis(DataRetriever.randBoolean())
+////                        .setThresholdLine(2, DataRetriever.randPaint())
+//                .setMaxAxisValue(100 * nSets, 20)//最大值 单位值
+//                .animate(DataRetriever.randAnimation(new Runnable() {
+//                    @Override
+//                    public void run() {
+//
+//                    }
+//                }, 3))
+//        //.show()
+//        ;
+//    }
 
     public void onInIt() {
         Toast.makeText(getActivity(), "我收到tab1微信的传召", Toast.LENGTH_SHORT).show();
