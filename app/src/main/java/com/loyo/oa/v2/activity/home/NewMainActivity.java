f464b5cb69b6cbf3fcaea6f0411e03f1fd472c62
应雨洋 <peiper341@gmail.com>
2016-06-17 15:54:40 +0800

    feature branch

--- app/src/main/java/com/loyo/oa/v2/activity/home/NewMainActivity.java
+++ app/src/main/java/com/loyo/oa/v2/activity/home/NewMainActivity.java
@@ -17,7 +17,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import com.jeremyfeinstein.slidingmenu.lib.SlidingMenu;
 import com.loyo.oa.v2.R;
 import com.loyo.oa.v2.activity.BulletinManagerActivity_;
