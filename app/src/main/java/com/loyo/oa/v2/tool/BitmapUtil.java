86856b9aa79cdf028f1130b72b4aa691c84b04bd
应雨洋 <peiper341@gmail.com>
2016-06-23 16:10:19 +0800

    附件预览持续优化

--- app/src/main/java/com/loyo/oa/v2/tool/BitmapUtil.java
+++ app/src/main/java/com/loyo/oa/v2/tool/BitmapUtil.java
@@ -13,7 +13,7 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.ImageView;
 
-import com.loyo.oa.v2.activity.PreviewImageActivity;
+import com.loyo.oa.v2.activity.ActivityPreviewImageList;
 import com.loyo.oa.v2.application.MainApp;
 import com.loyo.oa.v2.beans.Attachment;
 import com.loyo.oa.v2.common.FinalVariables;
@@ -509,7 +509,7 @@ public class BitmapUtil {
             bundle.putSerializable("data", newAttachment);
             bundle.putInt("position", newPosistion);
             bundle.putBoolean("isEdit", isEdit);
-            MainApp.getMainApp().startActivityForResult((Activity) mContext, PreviewImageActivity.class,
+            MainApp.getMainApp().startActivityForResult((Activity) mContext, ActivityPreviewImageList.class,
                     MainApp.ENTER_TYPE_BUTTOM, FinalVariables.REQUEST_DEAL_ATTACHMENT, bundle);
 
         }
