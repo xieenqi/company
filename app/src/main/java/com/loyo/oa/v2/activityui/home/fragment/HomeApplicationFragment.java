e6136b28c045dff3a424cd69926d7dd2d6ab4073
xeq <yjz@360loyo.com>
2016-07-15 11:16:21 +0800

    多次启动定位

--- app/src/main/java/com/loyo/oa/v2/activityui/home/fragment/HomeApplicationFragment.java
+++ app/src/main/java/com/loyo/oa/v2/activityui/home/fragment/HomeApplicationFragment.java
@@ -292,7 +292,7 @@ public class HomeApplicationFragment extends BaseFragment implements LocationUti
                     LogUtil.d(" Jpush user kongkong 空空");
                     setJpushAlias();
                     JpushCount++;
-                    LogUtil.d("Jpush 的链接状态:" + JPushInterface.getConnectionState(getActivity()));
+//                    LogUtil.d("Jpush 的链接状态:" + JPushInterface.getConnectionState(getActivity()));
                     isJPus = false;
                 }
             }, 5000);
