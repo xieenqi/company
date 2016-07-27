f67e6354240ce4ecb2c74a1e6d57993a6b04cfa5
xeq <yjz@360loyo.com>
2016-07-27 16:57:04 +0800

    客户 团队数据， 销售机会 团队数据 部门的筛选 返现问题

--- app/src/main/java/com/loyo/oa/v2/customview/ScreenDeptPopupView.java
+++ app/src/main/java/com/loyo/oa/v2/customview/ScreenDeptPopupView.java
@@ -16,12 +16,12 @@ import android.widget.PopupWindow;
 
 import com.loyo.oa.v2.R;
 import com.loyo.oa.v2.activityui.customer.bean.Role;
-import com.loyo.oa.v2.activityui.sale.adapter.AdapterSaleTeamScreen;
-import com.loyo.oa.v2.activityui.sale.bean.SaleTeamScreen;
 import com.loyo.oa.v2.activityui.other.bean.User;
+import com.loyo.oa.v2.activityui.sale.adapter.AdapterSaleTeamScreen1;
+import com.loyo.oa.v2.activityui.sale.adapter.AdapterSaleTeamScreen2;
+import com.loyo.oa.v2.activityui.sale.bean.SaleTeamScreen;
 import com.loyo.oa.v2.application.MainApp;
 import com.loyo.oa.v2.common.Common;
-import com.loyo.oa.v2.common.DialogHelp;
 import com.loyo.oa.v2.tool.LogUtil;
 
 import java.util.ArrayList;
@@ -45,8 +45,8 @@ public class ScreenDeptPopupView extends PopupWindow implements View.OnClickList
     private Bundle bundle;
     private boolean isKind;
 
-    private AdapterSaleTeamScreen adapter1;
-    private AdapterSaleTeamScreen adapter2;
+    private AdapterSaleTeamScreen1 adapter1;
+    private AdapterSaleTeamScreen2 adapter2;
     private List<SaleTeamScreen> depementData;
     private List<SaleTeamScreen> userData = new ArrayList<>();
     private ArrayList<User> deptAllUser = new ArrayList<>();
@@ -70,10 +70,11 @@ public class ScreenDeptPopupView extends PopupWindow implements View.OnClickList
         this.update();
         getFirstDept(0);
 
-        adapter1 = new AdapterSaleTeamScreen(context, depementData, 1);
+        adapter1 = new AdapterSaleTeamScreen1(context, depementData, 1);
         listView1.setAdapter(adapter1);
-        adapter2 = new AdapterSaleTeamScreen(context, userData, 2);
+        adapter2 = new AdapterSaleTeamScreen2(context, userData, 2);
         listView2.setAdapter(adapter2);
+        adapter2.refshData(0);
     }
 
     public void initView() {
@@ -83,7 +84,6 @@ public class ScreenDeptPopupView extends PopupWindow implements View.OnClickList
         cancel = (Button) contentView.findViewById(R.id.saleteam_screentag1_cancel);
         confirm.setOnClickListener(this);
         cancel.setOnClickListener(this);
-
         /*左侧列表*/
         listView1.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
@@ -92,7 +92,7 @@ public class ScreenDeptPopupView extends PopupWindow implements View.OnClickList
                 getFirstDept(position);
                 adapter1.selectPosition(position);
                 adapter1.notifyDataSetChanged();
-                adapter2.notifyDataSetChanged();
+                adapter2.refshData(position);
             }
         });
 
@@ -100,6 +100,8 @@ public class ScreenDeptPopupView extends PopupWindow implements View.OnClickList
         listView2.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                adapter2.selectPosition(position);
+                adapter2.notifyDataSetChanged();
                 resultData(position);
             }
         });
@@ -114,9 +116,9 @@ public class ScreenDeptPopupView extends PopupWindow implements View.OnClickList
         //设置全体人员 名字
         if (position == 0) {
             //如果数据权限为自己，则isKind: false请求数据传id true请求数据传xPath
-            if(MainApp.user.role.getDataRange() == Role.SELF){
+            if (MainApp.user.role.getDataRange() == Role.SELF) {
                 isKind = false;
-            }else{
+            } else {
                 isKind = true;
                 userData.get(position).setName(depementData.get(deptPosition).getName());
             }
@@ -151,7 +153,7 @@ public class ScreenDeptPopupView extends PopupWindow implements View.OnClickList
         saleTeamScreen.setId(depementData.get(position).getId());
 
         saleTeamScreen.setxPath(depementData.get(position).getxPath());
-        userData.add(0,saleTeamScreen);
+        userData.add(0, saleTeamScreen);
     }
 
     /*暂时弃用*/
