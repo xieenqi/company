8f9d84d653f1b37d66ca40f50e90a92b07fa1e56
应雨洋 <peiper341@gmail.com>
2016-08-02 10:55:22 +0800

    运力科技大数据，组织架构拉去缓慢，导致通讯录数据不完整崩溃，重新修改了对组织架构数据未获取完的判断逻辑

--- app/src/main/java/com/loyo/oa/v2/tool/Config_project.java
+++ app/src/main/java/com/loyo/oa/v2/tool/Config_project.java
@@ -6,15 +6,15 @@ package com.loyo.oa.v2.tool;
  */
 public class Config_project {
 
-    public static boolean isRelease = true;                               //是否是正式产品
+    public static boolean isRelease = true;                                //是否是正式产品
 
     public static final Boolean is_developer_mode = true;                  //生产模式
 
     //public static final Boolean is_developer_mode = BuildConfig.DEBUG;   //dbug模式
 
-   //public static String IP = "http://192.168.31.131";                    //内部测试环境
+    //public static String IP = "http://192.168.31.131";                    //内部测试环境
 
-    public static String IP = "http://staging.ukuaiqi.com";                //产品预上线环境
+    //public static String IP = "http://staging.ukuaiqi.com";                //产品预上线环境
 
     //public static String IP = "http://112.74.66.99";                     //产品预上线环境
 
@@ -22,7 +22,7 @@ public class Config_project {
 
     //public static String IP = "http://192.168.31.136";
 
-    //public static String IP = "http://192.168.31.155";
+    public static String IP = "http://192.168.31.139";
 
 
     protected Config_project() {
