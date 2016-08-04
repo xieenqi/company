537f0b90395723b29d1f78702d674332b760e745
xeq <yjz@360loyo.com>
2016-08-04 15:46:28 +0800

    订单详情 回款记录 附件 联通

---
+++ app/src/main/java/com/loyo/oa/v2/activityui/order/OrderPlanListActivity.java
@@ -0,0 +1,15 @@
+package com.loyo.oa.v2.activityui.order;
+
+import android.os.Bundle;
+
+import com.loyo.oa.v2.tool.BaseActivity;
+
+/** 回款计划 列表页面
+ * Created by xeq on 16/8/4.
+ */
+public class OrderPlanListActivity extends BaseActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
