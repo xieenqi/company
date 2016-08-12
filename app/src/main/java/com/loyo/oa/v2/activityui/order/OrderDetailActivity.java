82ca3cf5ff9073e78cd8742000cf3dd225e27c16
应雨洋 <peiper341@gmail.com>
2016-08-12 16:09:48 +0800

    设置非订单负责人，不能上传附件的权限

--- app/src/main/java/com/loyo/oa/v2/activityui/order/OrderDetailActivity.java
+++ app/src/main/java/com/loyo/oa/v2/activityui/order/OrderDetailActivity.java
@@ -187,6 +187,7 @@ public class OrderDetailActivity extends BaseActivity implements View.OnClickLis
                 break;
             case R.id.ll_enclosure://附件
                 mBundle = new Bundle();
+                mBundle.putBoolean(ExtraAndResult.EXTRA_ADD,isAdd);
                 mBundle.putString("uuid", mData.attachmentUUId);
                 app.startActivityForResult(this, OrderAttachmentActivity.class, MainApp.ENTER_TYPE_RIGHT, ExtraAndResult.MSG_WHAT_HIDEDIALOG, mBundle);
                 break;
