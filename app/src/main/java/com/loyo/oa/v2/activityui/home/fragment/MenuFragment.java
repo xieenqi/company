c0b517d5e07dd84f6127452233f4ab0e66cdbb9e
xeq <yjz@360loyo.com>
2016-08-17 11:24:05 +0800

    406 退出 多次调用接口 token无效个问题

--- app/src/main/java/com/loyo/oa/v2/activityui/home/fragment/MenuFragment.java
+++ app/src/main/java/com/loyo/oa/v2/activityui/home/fragment/MenuFragment.java
@@ -390,7 +390,7 @@ public class MenuFragment extends BaseFragment {
             }
         });
         //清楚token与用户资料
-        MainApp.setToken(null);
+//        MainApp.setToken(null);//避免后台多次调用接口 没有token 导致accst_token无效 的问题
         MainApp.user = null;
 
         //清楚本地登录状态
