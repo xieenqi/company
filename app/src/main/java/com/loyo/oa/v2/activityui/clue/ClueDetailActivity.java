7ddc6042af294ff235a1153fd594dc902b276be4
EthanGong <GQF@360loyo.com>
2016-08-25 17:18:29 +0800

    bugfix

--- app/src/main/java/com/loyo/oa/v2/activityui/clue/ClueDetailActivity.java
+++ app/src/main/java/com/loyo/oa/v2/activityui/clue/ClueDetailActivity.java
@@ -484,8 +484,7 @@ public class ClueDetailActivity extends BaseActivity implements View.OnClickList
                     @Override
                     public void success(Object o, Response response) {
                         HttpErrorCheck.checkResponse("【删除详情】线索：", response);
-                        app.finishActivity(ClueDetailActivity.this,MainApp.ENTER_TYPE_LEFT,RESULT_OK,new Intent());
-                        onBackPressed();
+                        app.finishActivity(ClueDetailActivity.this,MainApp.ENTER_TYPE_LEFT,ExtraAndResult.REQUEST_CODE,new Intent());
                     }
 
                     @Override
