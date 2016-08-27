4c26719059315595d1eebd00c286cbcc056d6b07
xeq <yjz@360loyo.com>
2016-08-27 15:42:40 +0800

    工单详情页面  manifest 整理

---
+++ app/src/main/java/com/loyo/oa/v2/activityui/worksheet/WorksheetDetailActivity.java
@@ -0,0 +1,10 @@
+package com.loyo.oa.v2.activityui.worksheet;
+
+import com.loyo.oa.v2.tool.BaseActivity;
+
+/**
+ * 【 工单详情 】  页面
+ * Created by xeq on 16/8/27.
+ */
+public class WorksheetDetailActivity extends BaseActivity {
+}
