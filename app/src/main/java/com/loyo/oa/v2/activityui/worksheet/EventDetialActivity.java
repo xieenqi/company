10b5ab5c60fd3be197503529d110c07c4baa3922
xeq <yjz@360loyo.com>
2016-08-31 17:41:25 +0800

    添加事件详情接口

--- app/src/main/java/com/loyo/oa/v2/activityui/worksheet/EventDetialActivity.java
+++ app/src/main/java/com/loyo/oa/v2/activityui/worksheet/EventDetialActivity.java
@@ -1,6 +1,8 @@
 package com.loyo.oa.v2.activityui.worksheet;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
@@ -9,7 +11,17 @@ import android.widget.TextView;
 import com.loyo.oa.v2.R;
 import com.loyo.oa.v2.application.MainApp;
 import com.loyo.oa.v2.common.ExtraAndResult;
+import com.loyo.oa.v2.common.http.HttpErrorCheck;
+import com.loyo.oa.v2.point.IWorksheet;
 import com.loyo.oa.v2.tool.BaseActivity;
+import com.loyo.oa.v2.tool.Config_project;
+import com.loyo.oa.v2.tool.RestAdapterFactory;
+
+import java.util.HashMap;
+
+import retrofit.Callback;
+import retrofit.RetrofitError;
+import retrofit.client.Response;
 
 /**
  * 【事件详情】页面
@@ -20,12 +32,24 @@ public class EventDetialActivity extends BaseActivity implements View.OnClickLis
     private TextView tv_title;
     private Button bt_confirm;
     private Bundle mBundle;
+    private String eventId, worksheetId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_detial);
+        getIntentData();
         initView();
+    }
+
+    private void getIntentData() {
+        Intent intent = getIntent();
+        eventId = intent.getStringExtra(ExtraAndResult.EXTRA_ID);
+        worksheetId = intent.getStringExtra(ExtraAndResult.EXTRA_ID2);
+        if (TextUtils.isEmpty(worksheetId) || TextUtils.isEmpty(eventId)) {
+            Toast("参数不全");
+            onBackPressed();
+        }
     }
 
     private void initView() {
@@ -35,6 +59,7 @@ public class EventDetialActivity extends BaseActivity implements View.OnClickLis
         bt_confirm.setOnClickListener(this);
         tv_title = (TextView) findViewById(R.id.tv_title);
         tv_title.setText("事件详情");
+        getData();
     }
 
 
@@ -47,11 +72,30 @@ public class EventDetialActivity extends BaseActivity implements View.OnClickLis
 
             case R.id.bt_confirm:
                 mBundle = new Bundle();
-                mBundle.putString(ExtraAndResult.CC_USER_ID,"57c63f25526f1552f2000003" /*事件id*/);
-                mBundle.putInt(ExtraAndResult.EXTRA_DATA,0x10 /*提交完成:0x01,打回重做0x02*/);
-                app.startActivity(this, WorksheetSubmitActivity.class, MainApp.ENTER_TYPE_RIGHT,false,mBundle);
+                mBundle.putString(ExtraAndResult.CC_USER_ID, "57c63f25526f1552f2000003" /*事件id*/);
+                mBundle.putInt(ExtraAndResult.EXTRA_DATA, 0x10 /*提交完成:0x01,打回重做0x02*/);
+                app.startActivity(this, WorksheetSubmitActivity.class, MainApp.ENTER_TYPE_RIGHT, false, mBundle);
                 break;
         }
+    }
+
+
+    private void getData() {
+        HashMap<String, Object> map = new HashMap<>();
+        map.put("wsId", worksheetId);
+        RestAdapterFactory.getInstance().build(Config_project.API_URL_STATISTICS()).create(IWorksheet.class).
+                getEventDetail(eventId, map, new Callback<Object>() {
+                    @Override
+                    public void success(Object o, Response response) {
+                        HttpErrorCheck.checkResponse("事件详细：", response);
+                    }
+
+                    @Override
+                    public void failure(RetrofitError error) {
+                        HttpErrorCheck.checkError(error);
+                    }
+                });
 
     }
+
 }
--- app/src/main/java/com/loyo/oa/v2/activityui/worksheet/common/WorksheetEventLayout.java
+++ app/src/main/java/com/loyo/oa/v2/activityui/worksheet/common/WorksheetEventLayout.java
@@ -54,7 +54,7 @@ public class WorksheetEventLayout extends LinearLayout {
         tv_name = (TextView) eventView.findViewById(R.id.tv_name);
         tv_time = (TextView) eventView.findViewById(R.id.tv_time);
         tv_content.setText(data.content);
-        tv_name.setText(data.responsor.getName());
+        tv_name.setText(null == data.responsor ? "未设置" : data.responsor.getName());
 
         if (MainApp.user.id.equals(data.responsorId))
             isresponsor = true;
@@ -104,6 +104,7 @@ public class WorksheetEventLayout extends LinearLayout {
             @Override
             public void onClick(View v) {
                 Message msg = new Message();
+                msg.obj = data.id;
                 msg.what = ExtraAndResult.REQUEST_CODE_CUSTOMER;
                 handler.sendMessage(msg);
             }
