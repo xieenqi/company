fcc9ed2aeab7942a308858ab0774d7fca614701a
xeq <yjz@360loyo.com>
2016-09-01 10:41:43 +0800

    事件详情 处理记录 功能 数据接入 附件展现完成

--- app/src/main/java/com/loyo/oa/v2/activityui/worksheet/common/EventHandleInfoList.java
+++ app/src/main/java/com/loyo/oa/v2/activityui/worksheet/common/EventHandleInfoList.java
@@ -5,8 +5,12 @@ import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.loyo.oa.v2.R;
+import com.loyo.oa.v2.activityui.worksheet.adapter.WorkSheetListNestingAdapter;
+import com.loyo.oa.v2.activityui.worksheet.bean.EventHandleInfoItem;
+import com.loyo.oa.v2.customview.CustomerListView;
 import com.loyo.oa.v2.customview.RoundImageView;
 
 /**
@@ -15,10 +19,12 @@ import com.loyo.oa.v2.customview.RoundImageView;
  */
 public class EventHandleInfoList extends LinearLayout {
     private RoundImageView iv_avatar;
+    private TextView tv_time, tv_content;
+    private CustomerListView lv_listview;
 
-    public EventHandleInfoList(Context context) {
+    public EventHandleInfoList(Context context, EventHandleInfoItem data) {
         super(context);
-        bindView(context);
+        bindView(context, data);
     }
 
     public EventHandleInfoList(Context context, AttributeSet attrs) {
@@ -30,9 +36,19 @@ public class EventHandleInfoList extends LinearLayout {
     }
 
 
-    private void bindView(Context context) {
+    private void bindView(Context context, EventHandleInfoItem data) {
         View view = LayoutInflater.from(context).inflate(R.layout.item_event_handler_info_list, null, false);
         iv_avatar = (RoundImageView) view.findViewById(R.id.iv_avatar);
+        tv_time = (TextView) view.findViewById(R.id.tv_time);
+        tv_content = (TextView) view.findViewById(R.id.tv_content);
+        lv_listview = (CustomerListView) view.findViewById(R.id.lv_listview);
+        tv_content.setText(data.content);
+        tv_time.setText(data.creatorName + "  " + data.createdAt + (data.type == 1 ? " 提交完成" : " 打回重做"));
+//        ImageLoader.getInstance().displayImage(data.);
+        if (null != data.attachments && data.attachments.size() > 0) {
+            WorkSheetListNestingAdapter mAdapter = new WorkSheetListNestingAdapter(data.attachments, context);
+            lv_listview.setAdapter(mAdapter);
+        }
         this.addView(view);
     }
 }
