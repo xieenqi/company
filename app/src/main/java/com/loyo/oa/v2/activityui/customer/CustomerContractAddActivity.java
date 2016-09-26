eaca05ec55d45abf4ed4135d406f0c9022fa8d44
应雨洋 <peiper341@gmail.com>
2016-09-26 15:17:48 +0800

    联系人电话与手机，空格处理

--- app/src/main/java/com/loyo/oa/v2/activityui/customer/CustomerContractAddActivity.java
+++ app/src/main/java/com/loyo/oa/v2/activityui/customer/CustomerContractAddActivity.java
@@ -101,26 +101,30 @@ public class CustomerContractAddActivity extends BaseActivity implements View.On
                         if (contactLeftExtras.fieldName.equals("name")) {
                             maps.put("name", contactLeftExtras.val);
                         } else if (contactLeftExtras.fieldName.equals("tel")) {
-//                            if (contactLeftExtras.required && contactLeftExtras.val.isEmpty()) {
-//                                Toast("手机号不能为空");
-//                                return;
-//                            }
-// else {
-//                                if (!RegularCheck.isMobilePhone(contactLeftExtras.val)) {
-//                                    Toast("手机号码格式不正确");
-//                                    return;
-//                                }
-//                            }
-                            maps.put("tel", contactLeftExtras.val);
+
+                           /* if (contactLeftExtras.required && contactLeftExtras.val.isEmpty()) {
+                                Toast("手机号不能为空");
+                                return;
+                            } else {
+                                if (!RegularCheck.isMobilePhone(contactLeftExtras.val)) {
+                                    Toast("手机号码格式不正确");
+                                    return;
+                                }
+                            }*/
+
+                            if(null != contactLeftExtras.val && !contactLeftExtras.val.isEmpty())
+                                maps.put("tel", contactLeftExtras.val.replaceAll(" +", ""));
 
                         } else if (contactLeftExtras.fieldName.equals("wiretel")) {
-//                            if (contactLeftExtras.required && TextUtils.isEmpty(contactLeftExtras.val)) {
-//                                Toast("座机号码不能为空");
-//                                return;
-////                                if (!RegularCheck.isPhone(contactLeftExtras.val)) {//验证座机号码
-////                                }
-//                            }
-                            maps.put("wiretel", contactLeftExtras.val);
+
+                            /*if (contactLeftExtras.required && TextUtils.isEmpty(contactLeftExtras.val)) {
+                                Toast("座机号码不能为空");
+                                return;
+                            }*/
+
+                            if(null != contactLeftExtras.val && !contactLeftExtras.val.isEmpty())
+                                maps.put("wiretel", contactLeftExtras.val.replaceAll(" +", ""));
+
                         } else if (contactLeftExtras.fieldName.equals("birth")) {
                             if (TextUtils.isEmpty(contactLeftExtras.val)) {
                                 try {
