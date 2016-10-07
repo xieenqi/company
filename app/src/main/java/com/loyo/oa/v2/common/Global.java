ef14ad849f6b5d547d9e09beadea93210a632824
xeq <yjz@360loyo.com>
2016-10-07 10:58:30 +0800

    合并代码

--- app/src/main/java/com/loyo/oa/v2/common/Global.java
+++ app/src/main/java/com/loyo/oa/v2/common/Global.java
@@ -349,7 +349,7 @@ public final class Global {
             resizedBitmap = Bitmap.createBitmap(bitmap, 0, 0,
                     bitmap.getWidth(), bitmap.getHeight(), matrix, true);
         } catch (OutOfMemoryError e) {
-            Global.Toast("图片过大旋转失败");
+            Global.Toast("图片过大旋转失败,请清理内存重试");
             return bitmap;
         }
         if (!bitmap.isRecycled()) {
