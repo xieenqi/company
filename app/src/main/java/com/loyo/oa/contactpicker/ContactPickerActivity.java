ab6c5d6a3458f04a58b7458449920855ee5e347a
EthanGong <ethangong@126.com>
2016-10-17 17:19:40 +0800

    搜索

--- app/src/main/java/com/loyo/oa/contactpicker/ContactPickerActivity.java
+++ app/src/main/java/com/loyo/oa/contactpicker/ContactPickerActivity.java
@@ -7,19 +7,25 @@ import android.content.IntentFilter;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.loyo.oa.contactpicker.adapter.PickDepartmentAdapter;
 import com.loyo.oa.contactpicker.adapter.PickUserAdapter;
 import com.loyo.oa.contactpicker.adapter.PickedContactsAdapter;
+import com.loyo.oa.contactpicker.adapter.SearchPickUserAdapter;
 import com.loyo.oa.contactpicker.callback.OnDepartmentSelected;
 import com.loyo.oa.contactpicker.callback.OnPickUserEvent;
 import com.loyo.oa.contactpicker.model.PickDepartmentModel;
@@ -31,12 +37,14 @@ import com.loyo.oa.indexablelist.adapter.expand.StickyRecyclerHeadersDecoration;
 import com.loyo.oa.indexablelist.widget.DividerDecoration;
 import com.loyo.oa.indexablelist.widget.ZSideBar;
 import com.loyo.oa.v2.R;
+import com.loyo.oa.v2.common.Global;
 import com.loyo.oa.v2.customview.HorizontalScrollListView;
 import com.loyo.oa.v2.db.OrganizationManager;
 import com.loyo.oa.v2.db.bean.DBDepartment;
 import com.loyo.oa.v2.db.bean.DBUser;
 import com.loyo.oa.v2.service.OrganizationService;
 import com.loyo.oa.v2.tool.BaseActivity;
+import com.loyo.oa.v2.tool.StringUtil;
 import com.pnikosis.materialishprogress.ProgressWheel;
 
 import java.util.ArrayList;
@@ -60,18 +68,22 @@ public class ContactPickerActivity extends BaseActivity implements View.OnClickL
     private CheckBox selectAllCheckBox;
     private RecyclerView departmentView;
     private RecyclerView userView;
+    private RecyclerView searchView;
     private ZSideBar zSideBar;
     private HorizontalScrollListView pickedView;
+    private EditText searchField;
 
     /* Adapter */
     private PickDepartmentAdapter departmentAdapter;
     private PickUserAdapter userAdapter;
+    private SearchPickUserAdapter searchAdapter;
     private PickedContactsAdapter pickedAdapter;
 
     /* Data*/
     private List<PickDepartmentModel> departments;
     private int selectedDepartmentIndex = 0;
     private PickedContacts pickedContacts;
+    private List<PickUserModel> searchBase = new ArrayList<>();
 
     /* Broadcasr */
     private BroadcastReceiver mReceiver = new BroadcastReceiver() {
@@ -151,6 +163,52 @@ public class ContactPickerActivity extends BaseActivity implements View.OnClickL
                 }
             }
         });
+
+        searchView = (RecyclerView) findViewById(R.id.search_user_view);
+        searchView.setLayoutManager(new LinearLayoutManager(this));
+        searchAdapter = new SearchPickUserAdapter(this);
+        searchView.setAdapter(searchAdapter);
+        searchAdapter.setCallback(this);
+
+        final StickyRecyclerHeadersDecoration headersDecor2 = new StickyRecyclerHeadersDecoration(searchAdapter);
+        searchView.addItemDecoration(headersDecor2);
+        searchView.addItemDecoration(new DividerDecoration(this));
+
+        searchField = (EditText) findViewById(R.id.search_user_field);
+        searchField.addTextChangedListener(new TextWatcher() {
+
+            @Override
+            public void onTextChanged(final CharSequence arg0, final int arg1, final int arg2, final int arg3) {
+            }
+
+            @Override
+            public void beforeTextChanged(final CharSequence arg0, final int arg1, final int arg2, final int arg3) {
+            }
+
+            @Override
+            public void afterTextChanged(final Editable s) {
+                if (!TextUtils.isEmpty(s.toString())) {
+                    doSearch(s.toString());
+                }
+                else {
+                    searchField.clearFocus();
+                }
+            }
+        });
+        searchField.setOnFocusChangeListener(new android.view.View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if(hasFocus) {
+                    searchView.setVisibility(View.VISIBLE);
+                }
+                else {
+                    searchView.setVisibility(View.INVISIBLE);
+                    InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
+                    searchAdapter.loadData(new ArrayList<PickUserModel>());
+                }
+            }
+        });
     }
 
     private void loadData() {
@@ -192,6 +250,10 @@ public class ContactPickerActivity extends BaseActivity implements View.OnClickL
         Iterator<DBUser> iterator = users.iterator();
         while (iterator.hasNext()) {
             result.add(PickUserModel.getPickModel(iterator.next()));
+        }
+
+        if (index == 0) {
+            searchBase.addAll(result);
         }
 
         userAdapter.loadData(result);
@@ -236,6 +298,37 @@ public class ContactPickerActivity extends BaseActivity implements View.OnClickL
         }
     }
 
+    void doSearch(String text) {
+        String key = text.trim();
+        if (StringUtil.isEmpty(key)) {
+            Global.Toast("请输入查询姓名!");
+            return;
+        }
+
+        List<PickUserModel> result = new ArrayList<>();
+
+        for (PickUserModel usrModel : searchBase) {
+            if (usrModel.user == null) {
+                continue;
+            } else if (usrModel.user.name != null && usrModel.user.name.contains(key)) {
+                result.add(usrModel);
+                continue;
+            } else if (usrModel.user.fullPinyin != null && usrModel.user.fullPinyin.startsWith(key)) {
+                result.add(usrModel);
+                continue;
+            } else if (usrModel.user.simplePinyin != null && usrModel.user.simplePinyin.startsWith(key)) {
+                result.add(usrModel);
+                continue;
+            }
+        }
+
+        if (result.size() == 0) {
+            Toast.makeText(this, "未搜索到相关结果", Toast.LENGTH_SHORT).show();
+        }
+
+        searchAdapter.loadData(result);
+    }
+
     /**
      * 部门列表
      */
@@ -277,6 +370,7 @@ public class ContactPickerActivity extends BaseActivity implements View.OnClickL
         pickedContacts.addUser(model);
         selectAllCheckBox.setSelected(departments.get(selectedDepartmentIndex).isSelected());
         pickedAdapter.loadData(pickedContacts.getPickedContacts());
+        searchAdapter.notifyDataSetChanged();
 
     }
 
@@ -285,25 +379,26 @@ public class ContactPickerActivity extends BaseActivity implements View.OnClickL
         pickedContacts.deleteUser(model);
         selectAllCheckBox.setSelected(departments.get(selectedDepartmentIndex).isSelected());
         pickedAdapter.loadData(pickedContacts.getPickedContacts());
+        searchAdapter.notifyDataSetChanged();
     }
 
     @Override
     public void onAddAllUsers(PickDepartmentModel model) {
-        Log.v("pickevent", "onAddAllUsers");
         model.setSelected(true);
         pickedContacts.addAllUsersOfDepartment(model);
         userAdapter.notifyDataSetChanged();
 
         pickedAdapter.loadData(pickedContacts.getPickedContacts());
+        searchAdapter.notifyDataSetChanged();
     }
 
     @Override
     public void onDeleteAllUsers(PickDepartmentModel model) {
-        Log.v("pickevent", "onDeleteAllUsers");
         model.setSelected(false);
         pickedContacts.deleteAllUserOfDepartment(model);
         userAdapter.notifyDataSetChanged();
 
         pickedAdapter.loadData(pickedContacts.getPickedContacts());
+        searchAdapter.notifyDataSetChanged();
     }
 }
