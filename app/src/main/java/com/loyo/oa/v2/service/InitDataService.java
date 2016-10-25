0f91a584a10504d2739985acc59981cdbef8a729
xeq <yjz@360loyo.com>
2016-10-25 14:49:40 +0800

    审批名称

--- app/src/main/java/com/loyo/oa/v2/service/InitDataService.java
+++ app/src/main/java/com/loyo/oa/v2/service/InitDataService.java
@@ -5,28 +5,21 @@ import android.content.Intent;
 import android.support.v4.content.LocalBroadcastManager;
 
 import com.loyo.oa.v2.activityui.commonview.bean.NewUser;
-import com.loyo.oa.v2.activityui.customer.bean.Department;
 import com.loyo.oa.v2.activityui.other.model.User;
 import com.loyo.oa.v2.application.MainApp;
 import com.loyo.oa.v2.beans.Permission;
-import com.loyo.oa.v2.common.Common;
-import com.loyo.oa.v2.common.ExtraAndResult;
 import com.loyo.oa.v2.common.FinalVariables;
-import com.loyo.oa.v2.common.Global;
 import com.loyo.oa.v2.common.event.AppBus;
 import com.loyo.oa.v2.common.http.HttpErrorCheck;
 import com.loyo.oa.v2.db.DBManager;
 import com.loyo.oa.v2.point.IUser;
 import com.loyo.oa.v2.tool.Config_project;
-import com.loyo.oa.v2.tool.ListUtil;
 import com.loyo.oa.v2.tool.LogUtil;
 import com.loyo.oa.v2.tool.RCallback;
 import com.loyo.oa.v2.tool.RestAdapterFactory;
-import com.loyo.oa.v2.tool.SharedUtil;
 
 import org.androidannotations.annotations.EIntentService;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 
 import retrofit.RetrofitError;
@@ -112,30 +105,30 @@ public class InitDataService extends IntentService {
         }
     }
 
-    /**
-     * <<<<<<< HEAD
-     * =======
-     * 后台 更新 组织架构
-     */
-    void getOrganization() {
-        ArrayList<Department> lstDepartment_current = RestAdapterFactory.getInstance().build(FinalVariables.GET_ORGANIZATION)
-                .create(IUser.class).getOrganization();
-
-        if (!ListUtil.IsEmpty(lstDepartment_current)) {
-            //写DB
-            DBManager.Instance().putOrganization(MainApp.gson.toJson(lstDepartment_current));
-            //设置缓存
-            Common.setLstDepartment(lstDepartment_current);
-            LogUtil.d("更新 组织《《《《《《《《《《《《《《《《》》》》》》》》》》》 架构 json：完成");
-            SharedUtil.remove(MainApp.getMainApp(), ExtraAndResult.IS_ORGANIZATION_UPDATE);
-            SharedUtil.remove(MainApp.getMainApp(), ExtraAndResult.APP_START);
-            //清除之前缓存通讯录部门的数据
-            SharedUtil.remove(MainApp.getMainApp(), ExtraAndResult.ORGANIZATION_DEPARTENT);
-        } else {
-            LogUtil.d("更新 组织 架构  失败");
-            Global.Toast("数据更新失败,稍后重试");
-        }
-    }
+//    /**
+//     * <<<<<<< HEAD
+//     * =======
+//     * 后台 更新 组织架构
+//     */
+//    void getOrganization() {
+//        ArrayList<Department> lstDepartment_current = RestAdapterFactory.getInstance().build(FinalVariables.GET_ORGANIZATION)
+//                .create(IUser.class).getOrganization();
+//
+//        if (!ListUtil.IsEmpty(lstDepartment_current)) {
+//            //写DB
+//            DBManager.Instance().putOrganization(MainApp.gson.toJson(lstDepartment_current));
+//            //设置缓存
+//            Common.setLstDepartment(lstDepartment_current);
+//            LogUtil.d("更新 组织《《《《《《《《《《《《《《《《》》》》》》》》》》》 架构 json：完成");
+//            SharedUtil.remove(MainApp.getMainApp(), ExtraAndResult.IS_ORGANIZATION_UPDATE);
+//            SharedUtil.remove(MainApp.getMainApp(), ExtraAndResult.APP_START);
+//            //清除之前缓存通讯录部门的数据
+//            SharedUtil.remove(MainApp.getMainApp(), ExtraAndResult.ORGANIZATION_DEPARTENT);
+//        } else {
+//            LogUtil.d("更新 组织 架构  失败");
+//            Global.Toast("数据更新失败,稍后重试");
+//        }
+//    }
 
     /**
      * >>>>>>> develop-bug-fix2
