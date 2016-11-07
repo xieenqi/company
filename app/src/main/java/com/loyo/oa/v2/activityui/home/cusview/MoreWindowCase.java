ed799b208619e2aa8e16baa0341891abcc655521
EthanGong <ethangong@126.com>
2016-11-07 13:01:26 +0800

    菜单动画优化

--- app/src/main/java/com/loyo/oa/v2/activityui/home/cusview/MoreWindowCase.java
+++ app/src/main/java/com/loyo/oa/v2/activityui/home/cusview/MoreWindowCase.java
@@ -1,8 +1,10 @@
 package com.loyo.oa.v2.activityui.home.cusview;
 
+import android.animation.Animator;
 import android.animation.ObjectAnimator;
 import android.animation.ValueAnimator;
 import android.app.Activity;
+import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
@@ -49,6 +51,7 @@ public class MoreWindowCase extends PopupWindow {
     private ArrayList<MoreWindowItem> data;
     private AdapterMoreWindow mAdapter;
     private GridView gridView;
+    private View bgView;
 
     private int mWidth;
     private int mHeight;
@@ -64,10 +67,20 @@ public class MoreWindowCase extends PopupWindow {
         context.getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
     }
 
+    public static int getStatusBarHeight(Context context) {
+        int result = 0;
+        int resourceId = context.getResources().getIdentifier("status_bar_height", "dimen",
+                "android");
+        if (resourceId > 0) {
+            result = context.getResources().getDimensionPixelSize(resourceId);
+        }
+        return result;
+    }
+
     public void init() {
         Rect frame = new Rect();
         mContext.getWindow().getDecorView().getWindowVisibleDisplayFrame(frame);
-        statusBarHeight = frame.top;
+        statusBarHeight = getStatusBarHeight(mContext);
         DisplayMetrics metrics = new DisplayMetrics();
         mContext.getWindowManager().getDefaultDisplay()
                 .getMetrics(metrics);
@@ -99,7 +112,7 @@ public class MoreWindowCase extends PopupWindow {
         canvas.scale(1 / scaleFactor, 1 / scaleFactor);
         Paint paint = new Paint();
         paint.setFlags(Paint.FILTER_BITMAP_FLAG);
-        canvas.drawBitmap(mBitmap, 0, 0, paint);
+        canvas.drawBitmap(mBitmap, 0, -statusBarHeight, paint);
 
         overlay = FastBlur.doBlur(overlay, (int) radius, true);
         return overlay;
@@ -139,6 +152,7 @@ public class MoreWindowCase extends PopupWindow {
         closeBtn = (Button) v.findViewById(R.id.case_music_window_close);
         closeBtn.setOnTouchListener(Global.GetTouch());
         gridView = (GridView) v.findViewById(R.id.case_gridview);
+        bgView = v.findViewById(R.id.bg_view);
 
         if (null == mAdapter) {
             mAdapter = new AdapterMoreWindow(data, mContext, mHandler);
@@ -154,7 +168,38 @@ public class MoreWindowCase extends PopupWindow {
             @Override
             public void onClick(View v) {
                 if (isShowing()) {
-                    dismiss();
+                    ValueAnimator alphaAnim = ObjectAnimator.ofFloat(bgView, "alpha", 1f, 0f);
+                    alphaAnim.setDuration(300);
+                    alphaAnim.start();
+                    ValueAnimator alphaAnim2 = ObjectAnimator.ofFloat(gridView, "alpha", 1f, 0f);
+                    alphaAnim2.setDuration(300);
+                    alphaAnim2.start();
+
+
+                    ValueAnimator rotateAnim = ObjectAnimator.ofFloat(closeBtn, "rotation", 0F, 45F);
+                    rotateAnim.setDuration(300);
+                    rotateAnim.start();
+                    rotateAnim.addListener(new Animator.AnimatorListener() {
+                        @Override
+                        public void onAnimationStart(Animator animation) {
+
+                        }
+
+                        @Override
+                        public void onAnimationEnd(Animator animation) {
+                            dismiss();
+                        }
+
+                        @Override
+                        public void onAnimationCancel(Animator animation) {
+
+                        }
+
+                        @Override
+                        public void onAnimationRepeat(Animator animation) {
+
+                        }
+                    });
                 }
             }
         });
@@ -218,10 +263,10 @@ public class MoreWindowCase extends PopupWindow {
         setContentView(view);
         initBtnUi(view);
         showAnimation();
-        setBackgroundDrawable(new BitmapDrawable(mContext.getResources(), blur()));
+        bgView.setBackgroundDrawable(new BitmapDrawable(mContext.getResources(), blur()));
         setOutsideTouchable(true);
         setFocusable(true);
-        showAtLocation(anchor, Gravity.BOTTOM, 0, statusBarHeight);
+        showAtLocation(anchor, Gravity.BOTTOM, 0, 0);
     }
 
     /**
@@ -229,27 +274,48 @@ public class MoreWindowCase extends PopupWindow {
      */
     private void showAnimation() {
 
-        mHandler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                ValueAnimator fadeAnim = ObjectAnimator.ofFloat(gridView, "translationY", 600, 0);
-                fadeAnim.setDuration(300);
-                KickBackAnimator kickAnimator = new KickBackAnimator();
-                kickAnimator.setDuration(150);
-                fadeAnim.setEvaluator(kickAnimator);
-                fadeAnim.start();
-            }
-        }, 50);
+//        mHandler.postDelayed(new Runnable() {
+//            @Override
+//            public void run() {
+//                ValueAnimator fadeAnim = ObjectAnimator.ofFloat(gridView, "translationY", 600, 0);
+//                fadeAnim.setDuration(300);
+//
+//                KickBackAnimator kickAnimator = new KickBackAnimator();
+//                kickAnimator.setDuration(150);
+//                fadeAnim.setEvaluator(kickAnimator);
+//                fadeAnim.start();
+//            }
+//        }, 50);
+
+//
+
 
         mHandler.postDelayed(new Runnable() {
             @Override
             public void run() {
-                ValueAnimator fadeAnim = ObjectAnimator.ofFloat(closeBtn, "translationY", 300, 0);
-                fadeAnim.setDuration(300);
-                KickBackAnimator kickAnimator = new KickBackAnimator();
-                kickAnimator.setDuration(150);
-                fadeAnim.setEvaluator(kickAnimator);
-                fadeAnim.start();
+
+            }
+        }, 100);
+
+        ValueAnimator alphaAnim = ObjectAnimator.ofFloat(bgView, "alpha", 0f, 1f);
+        alphaAnim.setDuration(300);
+        alphaAnim.start();
+
+        ValueAnimator rotateAnim = ObjectAnimator.ofFloat(closeBtn, "rotation", 45F, 0F);
+        rotateAnim.setDuration(300);
+        rotateAnim.start();
+
+        mHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+//                ValueAnimator fadeAnim = ObjectAnimator.ofFloat(closeBtn, "r", 300, 0);
+//                fadeAnim.setDuration(300);
+
+
+//                KickBackAnimator kickAnimator = new KickBackAnimator();
+//                kickAnimator.setDuration(150);
+//                fadeAnim.setEvaluator(kickAnimator);
+//                fadeAnim.start();
             }
         }, 100);
     }
