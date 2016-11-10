ac4f9224d3f78e22120170499fae188b9248bdfc
xeq <yjz@360loyo.com>
2016-11-10 14:09:22 +0800

    修复  商务电话导致异常奔溃问题

--- app/src/main/java/com/loyo/oa/v2/activityui/home/MainHomeActivity.java
+++ app/src/main/java/com/loyo/oa/v2/activityui/home/MainHomeActivity.java
@@ -94,9 +94,14 @@ public class MainHomeActivity extends SlidingFragmentActivity {
 
     @Subscribe
     public void onUserChanged(User user) {
-        if (! UCSService.isConnected()) {
-            VoIPManager.getInstance().connectVoipServer(null);
-        }
+        this.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                if (!UCSService.isConnected()) {
+                    VoIPManager.getInstance().connectVoipServer(null);
+                }
+            }
+        });
     }
 
     @Override
@@ -239,7 +244,7 @@ public class MainHomeActivity extends SlidingFragmentActivity {
         if (PackageManager.PERMISSION_GRANTED ==
                 getPackageManager().checkPermission("android.permission.RECORD_AUDIO", "com.loyo.oa.v2")) {
         } else {
-            ActivityCompat.requestPermissions(this,new String[] {Manifest.permission.RECORD_AUDIO},
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECORD_AUDIO},
                     1);
         }
     }
@@ -362,6 +367,6 @@ public class MainHomeActivity extends SlidingFragmentActivity {
                 stopService(new Intent(mContext, CheckUpdateService.class));
                 android.os.Process.killProcess(android.os.Process.myPid());
             }
-        },"提示",getString(R.string.app_exit_message));
+        }, "提示", getString(R.string.app_exit_message));
     }
 }
