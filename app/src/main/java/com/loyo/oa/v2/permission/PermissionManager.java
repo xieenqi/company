b92df13b730b07589abcc58f03b2a5b469f7037e
EthanGong <ethangong@126.com>
2016-11-29 11:17:10 +0800

    权限重构

---
+++ app/src/main/java/com/loyo/oa/v2/permission/PermissionManager.java
@@ -0,0 +1,39 @@
+package com.loyo.oa.v2.permission;
+
+import com.loyo.oa.v2.beans.Permission;
+
+import java.util.HashMap;
+
+/**
+ * Created by EthanGong on 2016/11/28.
+ */
+public class PermissionManager {
+    private static PermissionManager ourInstance = new PermissionManager();
+
+    public static PermissionManager getInstance() {
+        return ourInstance;
+    }
+
+    private HashMap<String, Permission> data = new HashMap<>();
+
+    private PermissionManager() {
+    }
+
+    public PermissionManager init(HashMap<String, Permission> map) {
+        if (map != null) {
+            data = map;
+        }
+        return this;
+    }
+
+    public boolean hasTeamPermission(@BusinessOperation.Type String module) {
+        
+
+
+        return true;
+    }
+
+    public int dateRange(@BusinessOperation.Type String mudule) {
+        return 0;
+    }
+}
