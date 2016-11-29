2bd18bcf92f8981b51cc3ca1f05800aae8c477d4
EthanGong <ethangong@126.com>
2016-11-29 11:26:08 +0800

    权限重构

--- app/src/main/java/com/loyo/oa/v2/permission/Permission.java
+++ app/src/main/java/com/loyo/oa/v2/permission/Permission.java
@@ -1,19 +1,31 @@
 package com.loyo.oa.v2.permission;
 
+import android.support.annotation.IntDef;
+
 import com.j256.ormlite.field.DatabaseField;
 import com.j256.ormlite.table.DatabaseTable;
 
 import java.io.Serializable;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 
 /**
  * Created by loyo_dev1 on 16/1/20.
  */
+
+// Updated by Ethan on 2016-11-29
+//        DATA_RANGE_ZERO = 0 //无意义的数据范围,如公告通知
+//        DATA_RANGE_ALL  = 1 // 公司可见
+//        DATA_RANGE_DEPT = 2 // 部门可见
+//        DATA_RANGE_SELF = 3 // 自己可见
+
 @DatabaseTable(tableName = "permission")
 public class Permission implements Serializable {
 
-    public static int COMPANY = 1;
-    public static int TEAM = 2;
-    public static int PERSONAL = 3;
+    public final static int ZERO     = 0;
+    public final static int COMPANY  = 1;
+    public final static int TEAM     = 2;
+    public final static int PERSONAL = 3;
     @DatabaseField
     public String name;
     @DatabaseField
@@ -46,6 +58,11 @@ public class Permission implements Serializable {
 
     public void setCode(String code) {
         this.code = code;
+    }
+
+    @IntDef({ZERO, COMPANY, TEAM, PERSONAL})
+    @Retention(RetentionPolicy.SOURCE)
+    public @interface DataRange {
     }
 
 }
