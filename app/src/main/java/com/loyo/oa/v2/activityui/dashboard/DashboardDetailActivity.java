e7fb2ca07539670adf7adf9b19db1b0f5f98eded
xeq <yjz@360loyo.com>
2016-12-12 16:30:54 +0800

    同步代码

---
+++ app/src/main/java/com/loyo/oa/v2/activityui/dashboard/DashboardDetailActivity.java
@@ -0,0 +1,19 @@
+package com.loyo.oa.v2.activityui.dashboard;
+
+import android.os.Bundle;
+
+import com.loyo.oa.v2.tool.BaseLoadingActivity;
+
+/**
+ * Created by xeq on 16/12/12.
+ */
+
+public class DashboardDetailActivity extends BaseLoadingActivity{
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
