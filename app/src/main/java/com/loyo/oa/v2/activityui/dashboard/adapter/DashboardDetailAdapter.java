c1e48635547f52f31ea30c8143cbdd31fa6a493c
xeq <yjz@360loyo.com>
2016-12-13 18:37:00 +0800

    sc

--- app/src/main/java/com/loyo/oa/v2/activityui/dashboard/adapter/DashboardDetailAdapter.java
+++ app/src/main/java/com/loyo/oa/v2/activityui/dashboard/adapter/DashboardDetailAdapter.java
@@ -7,6 +7,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.loyo.oa.v2.R;
@@ -28,7 +29,7 @@ public class DashboardDetailAdapter extends BaseAdapter {
 
     @Override
     public int getCount() {
-        return 20;
+        return 202;
     }
 
     @Override
@@ -53,7 +54,7 @@ public class DashboardDetailAdapter extends BaseAdapter {
             holder.text4 = (TextView) convertView.findViewById(R.id.text4);
             holder.view1 = convertView.findViewById(R.id.view1);
             holder.view2 = convertView.findViewById(R.id.view2);
-//            holder.item = (PercentRelativeLayout) convertView.findViewById(R.id.item);
+            holder.item = (LinearLayout) convertView.findViewById(R.id.item);
             convertView.setTag(holder);
         } else {
             holder = (Holder) convertView.getTag();
@@ -66,30 +67,34 @@ public class DashboardDetailAdapter extends BaseAdapter {
     class Holder {
         TextView text1, text2, text3, text4;
         View view1, view2;
-//        PercentRelativeLayout item;
+        LinearLayout item;
 
         public void setContent(int position) {
             int itemColor1 = Color.parseColor("#666666");
             int itemColor2 = Color.parseColor("#ff9900");
+            int itemColor3 = Color.parseColor("#333333");
             if (position == 0) {
-                text1.setTextColor(itemColor1);
-                text2.setTextColor(itemColor1);
-                text3.setTextColor(itemColor1);
-                text4.setTextColor(itemColor1);
+                setChildViewColor(itemColor1);
                 view1.setVisibility(View.VISIBLE);
                 view2.setVisibility(View.GONE);
             } else {
                 view1.setVisibility(View.GONE);
                 view2.setVisibility(View.VISIBLE);
+                setChildViewColor(itemColor3);
             }
             if (position == 1) {
-                text1.setTextColor(itemColor2);
-                text2.setTextColor(itemColor2);
-                text3.setTextColor(itemColor2);
-                text4.setTextColor(itemColor2);
+                setChildViewColor(itemColor2);
+            } else {
+                setChildViewColor(itemColor3);
             }
 
 
+        }
+
+        private void setChildViewColor(int color) {
+            for (int i = 0; i < item.getChildCount(); i++) {
+                ((TextView) item.getChildAt(i)).setTextColor(color);
+            }
         }
 
     }
