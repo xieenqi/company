6a1b65985337b911370ca3b6a5ba111eef048933
xeq <yjz@360loyo.com>
2016-12-19 16:07:07 +0800

    线索 下 的跟进动态列表 跟新评论bug fix

--- app/src/main/java/com/loyo/oa/v2/activityui/clue/viewcontrol/ClueFollowUpListView.java
+++ app/src/main/java/com/loyo/oa/v2/activityui/clue/viewcontrol/ClueFollowUpListView.java
@@ -5,6 +5,7 @@ import android.view.ViewGroup;
 import com.library.module.widget.loading.LoadingLayout;
 import com.loyo.oa.v2.activityui.clue.model.ClueFollowGroupModel;
 import com.loyo.oa.v2.activityui.clue.model.ClueFollowUpListModel;
+import com.loyo.oa.v2.activityui.signin.bean.CommentModel;
 import com.loyo.oa.v2.beans.PaginationX;
 
 /**
@@ -14,7 +15,7 @@ import com.loyo.oa.v2.beans.PaginationX;
 public interface ClueFollowUpListView {
 
     /*评论操作*/
-    void commentEmbl(String id);
+    void commentEmbl(String id,int parent, int chuild);
 
     /*删除附件操作*/
     void deleteCommentEmbl(String id);
@@ -23,7 +24,7 @@ public interface ClueFollowUpListView {
     void rushListData(boolean shw);
 
     /*发送评论成功操作*/
-    void commentSuccessEmbl();
+    void commentSuccessEmbl(CommentModel model);
 
     /*获取列表数据成功*/
     void getListDataSuccesseEmbl(PaginationX<ClueFollowGroupModel> paginationX);
