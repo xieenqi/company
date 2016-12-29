3de2854a3f41078ce252bd9090e8ad92199d2aa2
Mr.Jie <11447416@qq.com>
2016-12-29 18:29:37 +0800

    fixed
    Signed-off-by: Mr.Jie <11447416@qq.com>

--- app/src/main/java/com/loyo/oa/v2/activityui/dashboard/adapter/DashboardDetailAdapter.java
+++ app/src/main/java/com/loyo/oa/v2/activityui/dashboard/adapter/DashboardDetailAdapter.java
@@ -189,14 +189,14 @@ public class DashboardDetailAdapter extends BaseAdapter {
                 // 订单数量和金额
                 if (0 == position) {
                     text1.setText(records.get(0).name + "");
-                    text2.setText(records.get(0).orderNum + "");
-                    text3.setText(records.get(0).targetNum + "");
+                    text2.setText(records.get(0).targetNum + "");
+                    text3.setText(records.get(0).orderNum+ "");
                     text4.setText(records.get(0).finish_rate + "");
                 } else {
                     int tempP = position - 1;//添加了一个表头，消去下标偏移
                     text1.setText(records.get(tempP).name + "");
-                    text2.setText(records.get(tempP).orderNum + "");
-                    text3.setText(records.get(tempP).targetNum + "");
+                    text2.setText(records.get(tempP).targetNum + "");
+                    text3.setText(records.get(tempP).orderNum + "");
                     text4.setText(records.get(tempP).finish_rate + "");
 
                 }
