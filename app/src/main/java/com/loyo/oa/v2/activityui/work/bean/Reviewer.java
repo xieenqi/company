ac7796c043f84a350a913a580c2027a3f7d483d8
xeq <yjz@360loyo.com>
2016-12-29 18:57:45 +0800

    heb 合并

--- app/src/main/java/com/loyo/oa/v2/activityui/work/bean/Reviewer.java
+++ app/src/main/java/com/loyo/oa/v2/activityui/work/bean/Reviewer.java
@@ -16,7 +16,7 @@ public class Reviewer implements Serializable {
     public boolean viewed;//k看过吗
     public boolean reviewed;//点评过吗
     public long reviewedAt;//点评时间
-    public int score;//点评分数
+    public int score;//点评分数 (启用 16-12-29)
     public String comment;//点评内容
     public String status;
     public String newScore;//新的点评分数
