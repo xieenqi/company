252e0e36da9c18e82692b88c68cb411f6f7efe6b
EthanGong <ethangong@126.com>
2017-01-05 18:50:42 +0800

    bugfix

--- app/src/main/java/com/loyo/oa/v2/activityui/product/persenter/impl/AddBuProductPersenterImpl.java
+++ app/src/main/java/com/loyo/oa/v2/activityui/product/persenter/impl/AddBuProductPersenterImpl.java
@@ -13,10 +13,6 @@ import com.loyo.oa.v2.activityui.product.persenter.AddBuProductPersenter;
 import com.loyo.oa.v2.activityui.product.viewcontrol.AddBuProductView;
 import com.loyo.oa.v2.attachment.api.AttachmentService;
 import com.loyo.oa.v2.network.DefaultLoyoSubscriber;
-import com.loyo.oa.v2.network.LoyoErrorChecker;
-import com.loyo.oa.v2.network.model.BaseResponse;
-import com.loyo.oa.v2.tool.ListUtil;
-import com.loyo.oa.v2.tool.LogUtil;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -42,7 +38,7 @@ public class AddBuProductPersenterImpl implements AddBuProductPersenter {
     public void getProductDynm() {
         HashMap<String, Object> map = new HashMap<>();
         map.put("bizType",102);
-        ProductService.getProductDynm(map).subscribe(new DefaultLoyoSubscriber<ArrayList<ProductDynmModel>>() {
+        ProductService.getProductDynm(map).subscribe(new DefaultLoyoSubscriber<ArrayList<ProductDynmModel>>(ll_loading) {
             @Override
             public void onNext(ArrayList<ProductDynmModel> productDynmModel) {
                 addBuProductView.getDynmSuccessEmbl(productDynmModel);
