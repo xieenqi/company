db060acbc5eea8d9e5ff4cba52d54064b621d829
EthanGong <ethangong@126.com>
2017-01-05 16:18:34 +0800

    考勤 crash bugfix

--- app/src/main/java/com/loyo/oa/v2/activityui/attendance/fragment/AttendanceListFragment.java
+++ app/src/main/java/com/loyo/oa/v2/activityui/attendance/fragment/AttendanceListFragment.java
@@ -298,7 +298,7 @@ public class AttendanceListFragment extends BaseFragment implements View.OnClick
      * 跳转考勤界面，封装数据
      */
     public void intentValue() {
-        Intent intent = new Intent(getContext(), AttendanceAddActivity_.class);
+        Intent intent = new Intent(MainApp.getMainApp(), AttendanceAddActivity_.class);
         intent.putExtra("mAttendanceRecord", attendanceRecords);
         intent.putExtra("needPhoto", validateInfo.isNeedPhoto());
         intent.putExtra("isPopup", validateInfo.isPopup());
@@ -309,8 +309,6 @@ public class AttendanceListFragment extends BaseFragment implements View.OnClick
         intent.putExtra("earlyMin", attendanceRecords.getEarlyMin());
 
         startActivity(intent);
-        getActivity().overridePendingTransition(R.anim.enter_righttoleft, R.anim.exit_righttoleft);
-
     }
 
 
