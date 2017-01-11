7c11cf251658b44b9320fd6c52cbc1951be15ac3
Mr.Jie <11447416@qq.com>
2017-01-11 15:39:49 +0800

    项目，输入框颜色。
    Signed-off-by: Mr.Jie <11447416@qq.com>

--- app/src/main/java/com/loyo/oa/v2/activityui/work/WorkReportsInfoActivity.java
+++ app/src/main/java/com/loyo/oa/v2/activityui/work/WorkReportsInfoActivity.java
@@ -315,7 +315,7 @@ public class WorkReportsInfoActivity extends BaseActivity {
         OrganizationalMember reviewer = null != mWorkReport.reviewer && null != mWorkReport.reviewer.user ? mWorkReport.reviewer.user : null;
         tv_workContent.setText(TextUtils.isEmpty(mWorkReport.content) ? "无" : (mWorkReport.content.toString().contains("<") ? Html.fromHtml(mWorkReport.content) : mWorkReport.content));
         tv_reviewer.setText(mWorkReport.reviewer.user.getName());
-        tv_toUser.setText(getJoinUserNames().isEmpty() ? "抄送人：无" : getJoinUserNames());
+        tv_toUser.setText(getJoinUserNames().isEmpty() ? "无" : getJoinUserNames());
         tv_workReport_time.setText("提交时间：" + date);
 
         if (null != mWorkReport.ProjectInfo && mWorkReport.ProjectInfo.title.length() != 0) {
