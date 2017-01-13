352432d419f546a770828d84cb1779eab97a6e43
xeq <yjz@360loyo.com>
2017-01-13 10:49:05 +0800

    合并代码

--- app/src/main/java/com/loyo/oa/common/type/PushBizType.kt
+++ app/src/main/java/com/loyo/oa/common/type/PushBizType.kt
@@ -6,17 +6,26 @@ import java.lang.reflect.Type
 /** 推送的biztype
  * Created by xeq on 17/1/6.
  */
-enum class PushBizType constructor(var type: Int) {
+enum class PushBizType constructor(public var type: Int) {
     PUSH_TASKS(1);
 
     class PushBizTypeSerializer : JsonSerializer<PushBizType>, JsonDeserializer<PushBizType> {
 
         override fun deserialize(json: JsonElement?, typeOfT: Type?, context: JsonDeserializationContext?): PushBizType {
             throw UnsupportedOperationException("not implemented") //To change body of created functions use File | Settings | File Templates.
+            val list = PushBizType.values();
+            for (x in list.indices) {
+                if (list[x].type == json!!.asInt) {
+                    return list[x]
+                }
+            }
+
+
         }
 
         override fun serialize(src: PushBizType?, typeOfSrc: Type?, context: JsonSerializationContext?): JsonElement {
             throw UnsupportedOperationException("not implemented") //To change body of created functions use File | Settings | File Templates.
+            return JsonPrimitive(src!!.type)
         }
     }
 }
