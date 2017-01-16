d7592b3ae2882eed9c7961168194401f01137b05
EthanGong <ethangong@126.com>
2017-01-16 16:04:45 +0800

    bugfix: 审批终止不弹出提示框

--- app/src/main/java/com/loyo/oa/v2/activityui/order/OrderDetailActivity.java
+++ app/src/main/java/com/loyo/oa/v2/activityui/order/OrderDetailActivity.java
@@ -464,20 +464,7 @@ public class OrderDetailActivity extends BaseLoadingActivity implements View.OnC
             dialog.addSheetItem("意外终止", ActionSheetDialog.SheetItemColor.Red, new ActionSheetDialog.OnSheetItemClickListener() {
                 @Override
                 public void onClick(int which) {
-
-                    sweetAlertDialogView.alertHandle(new SweetAlertDialog.OnSweetClickListener() {
-                        @Override
-                        public void onClick(SweetAlertDialog sweetAlertDialog) {
-                            dismissSweetAlert();
-                        }
-                    }, new SweetAlertDialog.OnSweetClickListener() {
-                        @Override
-                        public void onClick(SweetAlertDialog sweetAlertDialog) {
-                            dismissSweetAlert();
-                            terminationOrder();
-                        }
-                    }, "提示", "  意外终止后，此订单无法再创建回款计划、回款记录，而且添加的回款记录也无法纳入业绩统计。" +
-                            "意外终止后不可恢复，你确定要终止吗？");
+                    terminationOrder();
                 }
             });
         if (isDelete)
@@ -525,15 +512,29 @@ public class OrderDetailActivity extends BaseLoadingActivity implements View.OnC
                     MainApp.ENTER_TYPE_RIGHT, ExtraAndResult.REQUEST_CODE_STAGE, mBundle);
         }
         else {
-            OrderService.terminationOrder(mData.id)
-                    .subscribe(new DefaultLoyoSubscriber<Object>() {
-                        @Override
-                        public void onNext(Object o) {
-                            Toast("订单终止成功");
-                            getData();
-                        }
-                    });
+            sweetAlertDialogView.alertHandle(new SweetAlertDialog.OnSweetClickListener() {
+                @Override
+                public void onClick(SweetAlertDialog sweetAlertDialog) {
+                    dismissSweetAlert();
+                }
+            }, new SweetAlertDialog.OnSweetClickListener() {
+                @Override
+                public void onClick(SweetAlertDialog sweetAlertDialog) {
+                    dismissSweetAlert();
+                    OrderService.terminationOrder(mData.id)
+                            .subscribe(new DefaultLoyoSubscriber<Object>() {
+                                @Override
+                                public void onNext(Object o) {
+                                    Toast("订单终止成功");
+                                    getData();
+                                }
+                            });
+                }
+            }, "提示", "  意外终止后，此订单无法再创建回款计划、回款记录，而且添加的回款记录也无法纳入业绩统计。" +
+                    "意外终止后不可恢复，你确定要终止吗？");
         }
+
+
     }
 
     @Override
