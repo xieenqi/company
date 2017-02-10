a9915c1d8ba90b9325bf8568286ec119a50579e2
EthanGong <ethangong@126.com>
2017-02-10 13:14:00 +0800

    丢公海提醒

---
+++ app/src/main/java/com/loyo/oa/v2/customermanagement/fragment/DropCustomerDeadlineFragment.java
@@ -0,0 +1,161 @@
+package com.loyo.oa.v2.customermanagement.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.DialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.loyo.oa.v2.R;
+import com.loyo.oa.v2.common.Global;
+import com.loyo.oa.v2.customermanagement.model.DropDeadlineModel;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+import static com.loyo.oa.v2.R.id.followup;
+
+/**
+ * Created by EthanGong on 2017/2/10.
+ */
+
+public class DropCustomerDeadlineFragment extends DialogFragment {
+
+    @BindView(R.id.tv_title) TextView titleText;
+    @BindView(R.id.tv_order_title) TextView orderTitleText;
+
+    @BindView(R.id.dim)                LinearLayout dimContainer;
+    @BindView(R.id.followup_container) LinearLayout followupContainer;
+    @BindView(R.id.order_container)    LinearLayout orderContainer;
+
+    @BindView(followup) LinearLayout followupView;
+    @BindView(R.id.visit)    LinearLayout visitView;
+    @BindView(R.id.call)     LinearLayout callView;
+    @BindView(R.id.order)    LinearLayout orderView;
+
+    @BindView(R.id.tv_followup_deadline) TextView followupDeadlineText;
+    @BindView(R.id.tv_followup) TextView followupDeadline;
+
+    @BindView(R.id.tv_visit_deadline) TextView visitDeadlineText;
+    @BindView(R.id.tv_visit) TextView visitDeadline;
+
+    @BindView(R.id.tv_call_deadline) TextView callDeadlineText;
+    @BindView(R.id.tv_call) TextView callDeadline;
+
+    @BindView(R.id.tv_order_deadline) TextView orderDeadlineText;
+    @BindView(R.id.tv_order) TextView orderDeadline;
+
+    @OnClick(R.id.dim) void onClose() {
+        dismiss();
+    }
+
+    @OnClick(R.id.tv_followup) void onFollowup() {
+
+    }
+
+    @OnClick(R.id.tv_visit) void onVisit() {
+
+    }
+
+    @OnClick(R.id.tv_call) void onCall() {
+
+    }
+
+    @OnClick(R.id.tv_order) void onOrder() {
+
+    }
+
+    DropDeadlineModel model;
+
+
+
+    public DropCustomerDeadlineFragment() {}
+
+    public static DropCustomerDeadlineFragment newInstance(DropDeadlineModel model) {
+        DropCustomerDeadlineFragment fragment = new DropCustomerDeadlineFragment();
+        fragment.setStyle(DialogFragment.STYLE_NORMAL, R.style.Dialog_FullScreen);
+        fragment.model = model;
+        return fragment;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_drop_customer_deadline, container);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        ButterKnife.bind(this, view);
+
+        followupDeadline.setOnTouchListener(Global.GetTouch());
+        visitDeadline.setOnTouchListener(Global.GetTouch());
+        callDeadline.setOnTouchListener(Global.GetTouch());
+        orderDeadline.setOnTouchListener(Global.GetTouch());
+
+        getDialog().getWindow().requestFeature(Window.FEATURE_NO_TITLE);
+        getDialog().getWindow()
+                .getAttributes().windowAnimations = R.style.DialogAnimation;
+
+    }
+
+    public void onResume() {
+
+        ViewGroup.LayoutParams params = getDialog().getWindow().getAttributes();
+        params.width = WindowManager.LayoutParams.MATCH_PARENT;
+        params.height = WindowManager.LayoutParams.MATCH_PARENT;
+        getDialog().getWindow().setAttributes((android.view.WindowManager.LayoutParams) params);
+        loadData();
+        super.onResume();
+    }
+
+    void loadData() {
+        titleText.setText("请在以下时间完成“"+ model.getConditionString() +"”跟进工作：");
+        if (model.activityRecycleAt > 0) {
+            followupContainer.setVisibility(View.VISIBLE);
+            orderTitleText.setText("且在以下时间完成签约：");
+            if (model.saleactRecycleAt > 0) {
+                followupView.setVisibility(View.VISIBLE);
+                followupDeadlineText.setText(DropDeadlineModel.formatDateTimeString(model.saleactRecycleAt));
+            }
+            else {
+                followupView.setVisibility(View.GONE);
+            }
+            if (model.visitRecycleAt > 0) {
+                visitView.setVisibility(View.VISIBLE);
+                visitDeadlineText.setText(DropDeadlineModel.formatDateTimeString(model.visitRecycleAt));
+            }
+            else {
+                visitView.setVisibility(View.GONE);
+            }
+            if (model.voiceRecycleAt > 0) {
+                callView.setVisibility(View.VISIBLE);
+                callDeadlineText.setText(DropDeadlineModel.formatDateTimeString(model.voiceRecycleAt));
+            }
+            else {
+                callView.setVisibility(View.GONE);
+            }
+        }
+        else {
+            followupContainer.setVisibility(View.GONE);
+            orderTitleText.setText("请在以下时间完成签约：");
+        }
+        if (model.orderRecycleAt > 0) {
+            orderContainer.setVisibility(View.VISIBLE);
+            orderDeadlineText.setText(DropDeadlineModel.formatDateTimeString(model.orderRecycleAt));
+        }
+        else
+        {
+            orderContainer.setVisibility(View.GONE);
+        }
+    }
+}
