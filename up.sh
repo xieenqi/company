af250777612c37b83c3c234f9b806758524cde54
Mr.Jie <11447416@qq.com>
2017-02-17 19:06:20 +0800

    添加默认测试包。
    Signed-off-by: Mr.Jie <11447416@qq.com>

--- up.sh
+++ up.sh
@@ -1,10 +1,23 @@
 #!/bin/sh
+#使用说明：直接在工程根目录，运行本文件，就可以自动上传默认的打包文件eg: ./sh
+#如果要上传指定的打包文件，就用：./up.sh 文件路径
+
+
 basepath=$(cd `dirname $0`; pwd)
-if [ $# -ne 1 ]
+ff=app/build/outputs/apk/app-debug.apk
+if
+[ $# -ne 1 ]
+[ ! -f $ff ];
 then
 	echo "输入1个上传文件地址"
+	exit
 else
-	filepath="$basepath/$1"
+    filepath="$basepath/$1"
+    if
+    [ $# -ne 1 ]
+    then
+    filepath=$ff
+    fi
 	if [ ! -f $filepath ]; then
    		echo "文件不存在！"
 	else
